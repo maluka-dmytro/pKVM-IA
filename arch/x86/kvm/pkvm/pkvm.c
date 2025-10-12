85838c6c28394f1b1efd0078b64872c21039aa69
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-12 10:04:53 +0800

    pKVM: x86: Add vcpu_put PV interface
    Add vcpu_put PV interface for the host to unload a guest vCPU via the
    vendor vcpu_put operation. As the current active vCPU is a host vCPU
    when involving this PV interface, loading the guest vCPU first before
    putting it in case the vcpu_put operation requires. After the guest vCPU
    is put, put the corresponding pkvm_vcpu instance.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -556,6 +556,53 @@ static int pkvm_vcpu_load(int vm_handle, int vcpu_handle)
 	return ret;
 }
 
+static int pkvm_vcpu_put(int vm_handle, int vcpu_handle)
+{
+	struct pkvm_vcpu *pkvm_vcpu = pkvm_get_vcpu(vm_handle, vcpu_handle);
+	int cpu = raw_smp_processor_id(), loaded_cpu, ret = 0;
+	struct kvm_vcpu *vcpu;
+
+	if (!pkvm_vcpu)
+		return -EINVAL;
+
+	vcpu = &pkvm_vcpu->vcpu;
+	loaded_cpu = cmpxchg(&vcpu->cpu, cpu, -1);
+	if (loaded_cpu == cpu) {
+		/*
+		 * The current active vCPU is the host vCPU. Switch to the guest
+		 * vCPU in case vcpu_put operation requires.
+		 */
+		kvm_x86_call(vcpu_load)(vcpu, cpu);
+
+		/*
+		 * Another guest vCPU may have already been loaded on this CPU
+		 * thus the cur_guest_vcpu may be overridden. So only set the
+		 * cur_guest_vcpu as NULL if it points to the guest vCPU being
+		 * put.
+		 */
+		if (vcpu == this_cpu_read(cur_guest_vcpu))
+			this_cpu_write(cur_guest_vcpu, NULL);
+
+		kvm_x86_call(vcpu_put)(vcpu);
+
+		/*
+		 * Put this pkvm_vcpu to allow it to be freed via the vcpu_free PV
+		 * interface.
+		 */
+		pkvm_put_vcpu(pkvm_vcpu);
+
+		/* Switch to the host vCPU as a guest vCPU was just loaded. */
+		switch_to_host_vcpu();
+	} else if (loaded_cpu != -1) {
+		/* The guest vCPU was not loaded on this CPU. */
+		ret = -EBUSY;
+	}
+
+	pkvm_put_vcpu(pkvm_vcpu);
+
+	return ret;
+}
+
 int pkvm_handle_host_hypercall(unsigned long nr, union pkvm_hc_data *in,
 			       union pkvm_hc_data *out)
 {
@@ -595,6 +642,9 @@ int pkvm_handle_host_hypercall(unsigned long nr, union pkvm_hc_data *in,
 		break;
 	case __pkvm__vcpu_load:
 		ret = pkvm_vcpu_load((int)in->val1, (int)in->val2);
+		break;
+	case __pkvm__vcpu_put:
+		ret = pkvm_vcpu_put((int)in->val1, (int)in->val2);
 		break;
 	default:
 		ret = -EINVAL;
