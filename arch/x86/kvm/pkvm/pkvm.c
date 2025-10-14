1506ac0cffc9d8a8ab5deff0d7d450d321b9c6dd
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-14 15:52:59 +0800

    pKVM: x86: Add set_cr0 PV interface
    Add set_cr0 PV interface to allow the host to set guest CR0 via the
    vendor-specific set_cr0 operation. This PV interface is enforced to
    ensure a pVM's CR0 cannot be changed by the host once the pVM has
    started running.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -628,6 +628,7 @@ static bool is_kvm_vcpu_accessible(struct kvm_vcpu *vcpu, unsigned long fn)
 	case __pkvm__cache_reg:
 	case __pkvm__set_cr4:
 	case __pkvm__post_set_cr3:
+	case __pkvm__set_cr0:
 		/*
 		 * As the host needs to pre-configure the pVM's vCPU state for
 		 * booting, the protection for pVM is only enforced by the pKVM
@@ -723,6 +724,11 @@ static void pkvm_post_set_cr3(struct pkvm_vcpu *pkvm_vcpu, unsigned long cr3)
 	kvm_x86_call(post_set_cr3)(&pkvm_vcpu->vcpu, cr3);
 }
 
+static void pkvm_set_cr0(struct pkvm_vcpu *pkvm_vcpu, unsigned long cr0)
+{
+	kvm_x86_call(set_cr0)(&pkvm_vcpu->vcpu, cr0);
+}
+
 static int pkvm_vcpu_handle_host_hypercall(unsigned long nr, union pkvm_hc_data *in,
 					   union pkvm_hc_data *out)
 {
@@ -762,6 +768,9 @@ static int pkvm_vcpu_handle_host_hypercall(unsigned long nr, union pkvm_hc_data 
 		break;
 	case __pkvm__post_set_cr3:
 		pkvm_post_set_cr3(pkvm_vcpu, in->val1);
+		break;
+	case __pkvm__set_cr0:
+		pkvm_set_cr0(pkvm_vcpu, (unsigned long)in->val1);
 		break;
 	default:
 		ret = -EINVAL;
