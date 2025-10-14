db66833ec377587976a650e3d31aec2895a69787
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-14 14:41:19 +0800

    pKVM: x86: Add event injection PV interfaces
    Add event injection PV interfaces for the host to inject irq, nmi and
    exceptions via vendor-specific operations.
    
    The PV interfaces for injecting irq/nmi are always accessible to the
    host as the host emulates the irq/nmi injections for both npVM and pVM.
    The exception for pVM is emulated by the pKVM, thus making this PV
    interface always inaccessible to the host.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/include/asm/pkvm_hypercalls.h
+++ arch/x86/include/asm/pkvm_hypercalls.h
@@ -54,6 +54,9 @@ PKVM_HC(interrupt_allowed)
 PKVM_HC(nmi_allowed)
 PKVM_HC_INOUT(get_nmi_mask)
 PKVM_HC(set_nmi_mask)
+PKVM_HC(inject_irq)
+PKVM_HC(inject_nmi)
+PKVM_HC(inject_exception)
 
 #undef PKVM_HC
 #undef PKVM_HC_INOUT
