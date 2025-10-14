9b110d319c05a97883138ad2b98f04f177f892e6
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-14 15:06:21 +0800

    pKVM: x86: Add sync_pir_to_irr PV interface
    Add sync_pir_to_irr PV interface for the host to sync the requested
    pir to the guest irr via the vendor-specific operation. This PV
    interface is always accessible to the host as it is part of guest irq
    emulation.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -852,6 +852,12 @@ static void pkvm_load_eoi_exitmap(struct kvm_vcpu *vcpu, u64 eoi_exit_bitmap0,
 	kvm_x86_call(load_eoi_exitmap)(vcpu, eoi_exit_bitmap);
 }
 
+static void pkvm_sync_pir_to_irr(struct kvm_vcpu *vcpu, int pir)
+{
+	to_pkvm_vcpu(vcpu)->max_irr = pir;
+	kvm_x86_call(sync_pir_to_irr)(vcpu);
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -999,6 +1005,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__hwapic_isr_update:
 		kvm_x86_call(hwapic_isr_update)(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__sync_pir_to_irr:
+		pkvm_sync_pir_to_irr(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	default:
 		ret = -EINVAL;
