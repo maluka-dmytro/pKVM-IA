e0ffd14a2fb4e8d0e10b68a90596fbc514a4ed65
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-14 14:48:25 +0800

    KVM: pVMX: Implement cancel injection operation
    Enable the pKVM VMX host to cancel injection for an irq, nmi and
    exception event via using the cancel_injection PV interface. The
    canceled event will be re-queued.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -776,6 +776,20 @@ static void pkvm_inject_exception(struct kvm_vcpu *vcpu)
 	pkvm_hypercall(inject_exception);
 }
 
+static void pkvm_cancel_injection(struct kvm_vcpu *vcpu)
+{
+	vcpu->arch.nmi_injected = false;
+	kvm_clear_exception_queue(vcpu);
+	kvm_clear_interrupt_queue(vcpu);
+
+	pkvm_hypercall(cancel_injection);
+
+	if (vcpu->arch.nmi_injected ||
+	    vcpu->arch.interrupt.injected ||
+	    vcpu->arch.exception.injected)
+		kvm_make_request(KVM_REQ_EVENT, vcpu);
+}
+
 static int pkvm_interrupt_allowed(struct kvm_vcpu *vcpu, bool for_injection)
 {
 	return pkvm_hypercall(interrupt_allowed, for_injection);
@@ -868,6 +882,7 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.inject_irq = pkvm_inject_irq,
 	.inject_nmi = pkvm_inject_nmi,
 	.inject_exception = pkvm_inject_exception,
+	.cancel_injection = pkvm_cancel_injection,
 	.interrupt_allowed = pkvm_interrupt_allowed,
 	.nmi_allowed = pkvm_nmi_allowed,
 	.get_nmi_mask = pkvm_get_nmi_mask,
