dab1f259eac52c7bcb0b763de20abd12c2c8c1c3
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-14 15:29:47 +0800

    pKVM: x86: Initialize KVM cpuid_xstate_sizes
    Initialize KVM cpuid_xstate_sizes, which will be needed by the pKVM to
    leverage KVM CPUID codes to emulate guest VM's CPUID.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/init_finalize.c
+++ arch/x86/kvm/pkvm/init_finalize.c
@@ -3,6 +3,7 @@
 #include <linux/extable.h>
 #include <asm/fpu/xstate.h>
 #include <asm/kvm_pkvm.h>
+#include "cpuid.h"
 #include "early_alloc.h"
 #include "fpu.h"
 #include "init_finalize.h"
@@ -252,6 +253,12 @@ static int finalize_global(struct pkvm_mem_info infos[], int nr_infos,
 		return ret;
 
 	pkvm_setup_xstate_cache();
+
+	/*
+	 * Iinitialize KVM cpuid_xstate_sizes to support CPUID emulation for the
+	 * guest VMs.
+	 */
+	kvm_init_xstate_sizes();
 
 	return hyp_g_finalize ? hyp_g_finalize() : 0;
 }
