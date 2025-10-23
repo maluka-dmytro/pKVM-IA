1daf3c29074da5e49dd673e77f78e0d51db2dc45
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-23 14:01:44 +0800

    pKVM: x86: Setup xstate cache
    Setup the xstate offset, size and flags in the pKVM hypervisor. These
    could be used by the pKVM hypervisor to locate the FPU register offset
    in the fpstate regs memory.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/init_finalize.c
+++ arch/x86/kvm/pkvm/init_finalize.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/kvm_host.h>
+#include <asm/fpu/xstate.h>
 #include <asm/kvm_pkvm.h>
 #include "early_alloc.h"
 #include "fpu.h"
@@ -233,6 +234,8 @@ static int finalize_global(struct pkvm_mem_info infos[], int nr_infos,
 	ret = create_host_mmu(tmp_infos, nr_infos, host_mmu_init_fn);
 	if (ret)
 		return ret;
+
+	pkvm_setup_xstate_cache();
 
 	return hyp_g_finalize ? hyp_g_finalize() : 0;
 }
