6e48e35a3f933f0749181c26102332c6cb0a7de7
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-28 09:55:51 +0800

    KVM: pVMX: Implement setup_mce operation
    Enable the pKVM VMX host to setup guest MCE via the setup_mce PV
    interface.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -1007,6 +1007,11 @@ static struct kvm_x86_nested_ops pkvm_nested_ops = {
 	.write_log_dirty = pkvm_nested_write_pml_buffer,
 };
 
+static void pkvm_setup_mce(struct kvm_vcpu *vcpu)
+{
+	pkvm_hypercall(setup_mce, vcpu->arch.mcg_cap);
+}
+
 static bool pkvm_apic_init_signal_blocked(struct kvm_vcpu *vcpu)
 {
 	/*
@@ -1110,6 +1115,8 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 
 	.pi_update_irte = vmx_pi_update_irte,
 	.pi_start_bypass = vmx_pi_start_bypass,
+
+	.setup_mce = pkvm_setup_mce,
 
 	.apic_init_signal_blocked = pkvm_apic_init_signal_blocked,
 
