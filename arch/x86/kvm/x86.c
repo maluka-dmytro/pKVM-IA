74ae71caa3a77db148aef45e6c38cac1ca40f78a
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-31 15:59:07 +0800

    pKVM: VMX: Handle EXIT_REASON_INVD
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -2332,6 +2332,7 @@ int kvm_emulate_wrmsr_imm(struct kvm_vcpu *vcpu, u32 msr, int reg)
 	return __kvm_emulate_wrmsr(vcpu, msr, kvm_register_read(vcpu, reg));
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_emulate_wrmsr_imm);
+#endif /* !__PKVM_HYP__ */
 
 int kvm_emulate_as_nop(struct kvm_vcpu *vcpu)
 {
@@ -2354,6 +2355,7 @@ fastpath_t handle_fastpath_invd(struct kvm_vcpu *vcpu)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(handle_fastpath_invd);
 
+#ifndef __PKVM_HYP__
 int kvm_handle_invalid_op(struct kvm_vcpu *vcpu)
 {
 	kvm_queue_exception(vcpu, UD_VECTOR);
