aa26eb3a73783379a5e9a0b1ea6e7cc7c41f0aaf
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-31 18:46:34 +0800

    pKVM: VMX: Handle EXIT_REASON_BUS_LOCK
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -6595,7 +6595,6 @@ static int handle_encls(struct kvm_vcpu *vcpu)
 }
 #endif /* CONFIG_X86_SGX_KVM */
 
-#ifndef __PKVM_HYP__
 static int handle_bus_lock_vmexit(struct kvm_vcpu *vcpu)
 {
 	/*
@@ -6607,6 +6606,7 @@ static int handle_bus_lock_vmexit(struct kvm_vcpu *vcpu)
 	return 1;
 }
 
+#ifndef __PKVM_HYP__
 static int handle_notify(struct kvm_vcpu *vcpu)
 {
 	unsigned long exit_qual = vmx_get_exit_qual(vcpu);
@@ -6758,8 +6758,8 @@ static int (*kvm_vmx_exit_handlers[])(struct kvm_vcpu *vcpu) = {
 	[EXIT_REASON_PREEMPTION_TIMER]	      = handle_preemption_timer,
 #endif
 	[EXIT_REASON_ENCLS]		      = handle_encls,
-#ifndef __PKVM_HYP__
 	[EXIT_REASON_BUS_LOCK]                = handle_bus_lock_vmexit,
+#ifndef __PKVM_HYP__
 	[EXIT_REASON_NOTIFY]		      = handle_notify,
 	[EXIT_REASON_MSR_READ_IMM]            = handle_rdmsr_imm,
 	[EXIT_REASON_MSR_WRITE_IMM]           = handle_wrmsr_imm,
