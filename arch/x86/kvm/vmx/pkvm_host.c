74ed523dcff3f8412175d2ae07c31b293d131125
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-31 13:11:49 +0800

    pKVM: VMX: Handle EXIT_REASON_INTERRUPT_WINDOW
    The EXIT_REASON_NMI_WINDOW is handled cooperatively by the pKVM
    hypervisor and the host. The pKVM hypervisor closes the IRQ window and
    then return back to the host to check the next IRQ event if there is.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -338,6 +338,14 @@ static int handle_dr(struct kvm_vcpu *vcpu)
 	return 1;
 }
 
+static int handle_interrupt_window(struct kvm_vcpu *vcpu)
+{
+	kvm_make_request(KVM_REQ_EVENT, vcpu);
+
+	++vcpu->stat.irq_window_exits;
+	return 1;
+}
+
 /*
  * The exit handlers return 1 if the exit was handled fully and guest execution
  * may resume.  Otherwise they set the kvm_run parameter to indicate what needs
@@ -352,6 +360,7 @@ static int (*pkvm_vmx_exit_handlers[])(struct kvm_vcpu *vcpu) = {
 	[EXIT_REASON_DR_ACCESS]               = handle_dr,
 	[EXIT_REASON_MSR_READ]                = kvm_emulate_rdmsr,
 	[EXIT_REASON_MSR_WRITE]               = kvm_emulate_wrmsr,
+	[EXIT_REASON_INTERRUPT_WINDOW]        = handle_interrupt_window,
 };
 
 static const int pkvm_vmx_max_exit_handlers = ARRAY_SIZE(pkvm_vmx_exit_handlers);
