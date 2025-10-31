f25448b598d9bdbaeb5b912844e4a723eb1378b8
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-31 18:49:04 +0800

    pKVM: VMX: Handle EXIT_REASON_NOTIFY
    Enable the instruction timeout feature if this is supported and
    requested by the host. The exit is handled by the pKVM hypervisor first
    if the exit hanppened while executing iret from NMI, then returns back
    to the host from the vcpu_run PV interface, which will handle this
    vmexit according similarly with the native KVM VMX host.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -348,6 +348,13 @@ static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate 
 	    kvm_caps.has_bus_lock_exit)
 		kvm->arch.bus_lock_detection_enabled = true;
 
+	if (!kvm->arch.notify_vmexit_flags &&
+	    pkvm_vm->shared_kvm->arch.notify_vmexit_flags &&
+	    kvm_caps.has_notify_vmexit) {
+		kvm->arch.notify_window = pkvm_vm->shared_kvm->arch.notify_window;
+		kvm->arch.notify_vmexit_flags = pkvm_vm->shared_kvm->arch.notify_vmexit_flags;
+	}
+
 	pkvm_spin_unlock(&pkvm_vm->lock);
 
 	vcpu->kvm = kvm;
