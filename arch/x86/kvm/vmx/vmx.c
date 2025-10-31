924368a1efd099ab8dd809415183c981df856181
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-10-31 21:33:38 +0800

    pKVM: x86: Handle pVM's IO hypercalls
    The pVM's IO hypercalls are used by the pVM to access devices' MMIO
    which are emulated by the host. So the hypercalls are emulated by the
    pKVM hypervisor to skip the instructions, and eventually handled by the
    host via leveraging the kvm_sev_es_mmio_read/write() helpers.
    
    To guarantee the vCPU state protection for the pVM, only necessary vCPU
    state are shared with the host (i.e., sharing the RAX/RCX/RDX/RBX/RSI
    with the host, and updating the RAX with the result from the host).
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -9714,6 +9714,16 @@ static void update_protected_vcpu_state(struct kvm_vcpu *vcpu,
 		WARN_ON_ONCE(kvm_complete_insn_gp(vcpu,
 				xchg(&to_pkvm_vcpu(vcpu)->host_emulated_msr_err, 0)) != 1);
 		break;
+	case EXIT_REASON_VMCALL:
+		/*
+		 * After a hypercall being emulated by the host, the RAX
+		 * may be filled by the host with the return value to
+		 * the guest. So for the pVM, suppose it awares that the
+		 * RAX may be modified by the host after returning back
+		 * form a hypercall.
+		 */
+		kvm_rax_write(vcpu, shared_vcpu->arch.regs[VCPU_REGS_RAX]);
+		break;
 	default:
 		break;
 	}
@@ -9801,6 +9811,22 @@ static void share_protected_vcpu_state(struct kvm_vcpu *vcpu,
 	case EXIT_REASON_MSR_READ_IMM:
 		shared_vcpu->arch.regs[VCPU_REGS_RCX] = kvm_rcx_read(vcpu);
 		to_vmx(shared_vcpu)->instr_info = vmcs_read32(VMX_INSTRUCTION_INFO);
+		break;
+	case EXIT_REASON_VMCALL:
+		/*
+		 * The pVM may also needs to use the hypercall to
+		 * communicate with the host, e.g., MMIO emulation. And
+		 * the hypercall parameters are usually filled by the
+		 * pVM software in below registers, so they should be
+		 * shared with the host. Suppose the pVM itself won't
+		 * leak any sensitive data via below registers when
+		 * fills the hypercall parameters.
+		 */
+		shared_vcpu->arch.regs[VCPU_REGS_RAX] = kvm_rax_read(vcpu);
+		shared_vcpu->arch.regs[VCPU_REGS_RCX] = kvm_rcx_read(vcpu);
+		shared_vcpu->arch.regs[VCPU_REGS_RDX] = kvm_rdx_read(vcpu);
+		shared_vcpu->arch.regs[VCPU_REGS_RBX] = kvm_rbx_read(vcpu);
+		shared_vcpu->arch.regs[VCPU_REGS_RSI] = kvm_rsi_read(vcpu);
 		break;
 	default:
 		break;
