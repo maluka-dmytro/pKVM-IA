1a14c0da75c18550484a5027c5f1fe63655a3445
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-09 15:26:26 +0800

    KVM: pVMX: Implement disable_virtualization_cpu operation
    The disable_virtualization_cpu operation is mandatory but the pKVM
    hypervisor doesn't support disabling VMX via this PV interface due to
    security concerns. As a result, the CPU will remain in VMX non-root mode
    during shutting down or rebooting if there is no hardware level reset.
    In this case, if INIT-SIPI are send, such events will cause vmexits to
    the pKVM hypervisor rather than the operations associated with these
    events. Handling such vmexits is not supported by the pKVM hypervisor.
    
    The same to emergency_disable_virtualization_cpu operation. Thus these
    two operations share the same stub function.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -12,10 +12,25 @@ static int pkvm_enable_virtualization_cpu(void)
 	return pkvm_hypercall(enable_virtualization_cpu);
 }
 
+static void pkvm_disable_virtualization_cpu(void)
+{
+	/*
+	 * The pKVM hypervisor doesn't support disabling VMX via this PV
+	 * interface due to security concerns. As a result, the CPU will remain
+	 * in VMX non-root mode during shutting down or rebooting if there is no
+	 * hardware level reset. In this case, if INIT-SIPI are send, they will
+	 * cause vmexits to the pKVM hypervisor rather than the operations
+	 * associated with these events. Handling such vmexits is not supported
+	 * by the pKVM hypervisor.
+	 */
+}
+
 struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.name = KBUILD_MODNAME,
 
 	.check_processor_compatibility = pkvm_check_processor_compat,
 
 	.enable_virtualization_cpu = pkvm_enable_virtualization_cpu,
+	.disable_virtualization_cpu = pkvm_disable_virtualization_cpu,
+	.emergency_disable_virtualization_cpu = pkvm_disable_virtualization_cpu,
 };
