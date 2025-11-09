36fad90cecc6ae176372d73d16f3289b803c2c64
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-09 15:56:09 +0800

    KVM: pVMX: Implement enabling irq/nmi window operations
    Enable the pKVM VMX host to enable irq/nmi window for a guest via the PV
    interfaces.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -750,6 +750,16 @@ static u32 pkvm_get_interrupt_shadow(struct kvm_vcpu *vcpu)
 	return out.get_interrupt_shadow.data;
 }
 
+static void pkvm_enable_nmi_window(struct kvm_vcpu *vcpu)
+{
+	pkvm_hypercall(enable_nmi_window);
+}
+
+static void pkvm_enable_irq_window(struct kvm_vcpu *vcpu)
+{
+	pkvm_hypercall(enable_irq_window);
+}
+
 struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 	.name = KBUILD_MODNAME,
 
@@ -806,6 +816,8 @@ struct kvm_x86_ops pkvm_host_vt_x86_ops __initdata = {
 
 	.set_interrupt_shadow = pkvm_set_interrupt_shadow,
 	.get_interrupt_shadow = pkvm_get_interrupt_shadow,
+	.enable_nmi_window = pkvm_enable_nmi_window,
+	.enable_irq_window = pkvm_enable_irq_window,
 };
 
 bool pkvm_interrupt_blocked(struct kvm_vcpu *vcpu)
