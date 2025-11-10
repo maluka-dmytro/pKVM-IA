d27e5d74f29e8c0413ebd1161c9f31c0cfd47f28
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 17:40:58 +0800

    pKVM: VMX: Implement set_msr operation
    Import vmx_set_msr() from KVM for pKVM to implement set_msr operation
    with some unsupported MSRs being disabled.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -668,14 +668,15 @@ void kvm_user_return_msr_cpu_online(void)
 	}
 }
 
-#ifndef __PKVM_HYP__
 static void kvm_user_return_register_notifier(struct kvm_user_return_msrs *msrs)
 {
+#ifndef __PKVM_HYP__
 	if (!msrs->registered) {
 		msrs->urn.on_user_return = kvm_on_user_return;
 		user_return_notifier_register(&msrs->urn);
 		msrs->registered = true;
 	}
+#endif
 }
 
 int kvm_set_user_return_msr(unsigned slot, u64 value, u64 mask)
@@ -696,6 +697,7 @@ int kvm_set_user_return_msr(unsigned slot, u64 value, u64 mask)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_set_user_return_msr);
 
+#ifndef __PKVM_HYP__
 void kvm_user_return_msr_update_cache(unsigned int slot, u64 value)
 {
 	struct kvm_user_return_msrs *msrs = this_cpu_ptr(user_return_msrs);
@@ -14134,6 +14136,7 @@ void kvm_arch_gmem_invalidate(kvm_pfn_t start, kvm_pfn_t end)
 }
 #endif
 #endif
+#endif /* !__PKVM_HYP__ */
 
 int kvm_spec_ctrl_test_value(u64 value)
 {
@@ -14161,6 +14164,7 @@ int kvm_spec_ctrl_test_value(u64 value)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_spec_ctrl_test_value);
 
+#ifndef __PKVM_HYP__
 void kvm_fixup_and_inject_pf_error(struct kvm_vcpu *vcpu, gva_t gva, u16 error_code)
 {
 	struct kvm_mmu *mmu = vcpu->arch.walk_mmu;
