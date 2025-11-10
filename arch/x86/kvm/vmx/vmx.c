e312b7a714bb01e4ef3e23f84453c17856b44f06
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-11-10 18:03:23 +0800

    pKVM: VMX: Implement write_tsc_offset/multiplier operations
    Import vmx_write_tsc_offset/multiplier() from KVM to implement
    write_tsc_offset/multiplier operations for pKVM.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -2008,6 +2008,7 @@ u64 vmx_get_l2_tsc_multiplier(struct kvm_vcpu *vcpu)
 
 	return kvm_caps.default_tsc_scaling_ratio;
 }
+#endif /* !__PKVM_HYP__ */
 
 void vmx_write_tsc_offset(struct kvm_vcpu *vcpu)
 {
@@ -2018,7 +2019,6 @@ void vmx_write_tsc_multiplier(struct kvm_vcpu *vcpu)
 {
 	vmcs_write64(TSC_MULTIPLIER, vcpu->arch.tsc_scaling_ratio);
 }
-#endif /* !__PKVM_HYP__ */
 
 /*
  * Userspace is allowed to set any supported IA32_FEATURE_CONTROL regardless of
