c5c31104599135b14a412db5536732473fc067d3
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-12-05 15:52:38 +0800

    pKVM: x86: Enforce initial values of most MSRs
    Even though most MSRs are emulated by pKVM itself, not by the host,
    pKVM still allows the host to set initial values of any MSRs for pVMs,
    which is a potential source of security issues.
    
    So implement a simple policy similar to the one we implemented for
    initial CPUID values: allow the host to pre-initialize only those
    MSRs that are currently pre-initialized by crosvm.
    
    Additionally allow the host to set any MSR to the same value it is
    already set to by pKVM (in such case there's nothing to do anyway, so
    just return success to the host instead of failure). This addresses
    cases like KVM-high setting MSR_IA32_XSS to 0 in kvm_vcpu_reset() or
    crosvm setting MSR_STAR, MSR_CSTAR etc to 0.
    
    MSRs that are managed by the host (e.g. APIC, TSC) are not affected,
    since the host doesn't need to ask pKVM to set them anyway.
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -608,6 +608,56 @@ static void pkvm_update_exception_bitmap(struct kvm_vcpu *vcpu)
 	kvm_x86_call(update_exception_bitmap)(vcpu);
 }
 
+static int pkvm_set_msr(struct kvm_vcpu *vcpu, u32 index, u64 data)
+{
+	if (pkvm_is_protected_vcpu(vcpu)) {
+		u64 cur_data;
+		int ret;
+
+		if (kvm_vcpu_has_run(vcpu))
+			return -EPERM;
+
+		/*
+		 * For simplicity and security, allow the host to change
+		 * initial values of those MSRs (or individual bits in MSRs)
+		 * that are currently tweaked by crosvm, and only those.
+		 * The allowed set can be extended as needed.
+		 */
+		switch (index) {
+		case MTRRphysBase_MSR(0) ... MSR_MTRRfix4K_F8000:
+		case MSR_MTRRdefType:
+			break;
+		case MSR_IA32_MISC_ENABLE:
+			if (data & ~(MSR_IA32_MISC_ENABLE_FAST_STRING |
+				     MSR_IA32_MISC_ENABLE_PEBS_UNAVAIL |
+				     MSR_IA32_MISC_ENABLE_BTS_UNAVAIL))
+				return -EPERM;
+
+			/*
+			 * vPMU is not supported by pKVM yet. Don't trick the pVM
+			 * that it is.
+			 */
+			data |= MSR_IA32_MISC_ENABLE_PEBS_UNAVAIL |
+				MSR_IA32_MISC_ENABLE_BTS_UNAVAIL;
+			break;
+		default:
+			/*
+			 * Allow the host to set an MSR to a value to which it is
+			 * already set by pKVM anyway, i.e. if setting is a no-op.
+			 */
+			ret = kvm_msr_read(vcpu, index, &cur_data);
+			if (ret)
+				return ret;
+			if (data == cur_data)
+				return 0;
+
+			return -EPERM;
+		}
+	}
+
+	return kvm_msr_write(vcpu, index, data);
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *out)
 {
@@ -630,8 +680,8 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		ret = kvm_x86_call(set_efer)(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	case __pkvm__set_msr:
-		ret = kvm_msr_write(vcpu, pkvm_hc_input1(hvcpu),
-				    pkvm_hc_input2(hvcpu));
+		ret = pkvm_set_msr(vcpu, pkvm_hc_input1(hvcpu),
+				   pkvm_hc_input2(hvcpu));
 		break;
 	case __pkvm__get_msr:
 		ret = kvm_msr_read(vcpu, pkvm_hc_input1(hvcpu), &out->get_msr.data);
