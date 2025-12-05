81f914069e2d705039a457b31d483528fbb836b5
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-12-05 15:23:14 +0800

    pKVM: x86: Add irq/nmi mask PV interfaces
    Add irq/nmi mask PV interfaces for the host to access irq/nmi mask via
    vendor-specific operations. Specifically, __pkvm__interrupt/nmi_allowed
    are for the host to check if irq/nmi is allowed or not, for injection or
    non-injection, and __pkvm__get/set_nmi_mask are for the host to control
    the guest's NMI mask.
    
    As pVM's interrupt are virtualized by the host, the host still needs to
    check for irq/nmi allowance or nmi mask for injection. The PV interfaces
    __pkvm__interrupt/nmi_allowed, and __pkvm__get_nmi_mask are not
    enforced.
    
    As set_nmi_mask is used to emulate, i.e., SMM mode, real mode iret and
    setting segment by the host. None of these cases are supported for the
    pVM. So making __pkvm__set_nmi_mask inaccessible to the host once the
    pVM started running to avoid the host changing pVM's NMI mask without a
    real need.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -619,6 +619,7 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__flush_tlb_guest:
 	case __pkvm__set_interrupt_shadow:
 	case __pkvm__get_interrupt_shadow:
+	case __pkvm__set_nmi_mask:
 		/*
 		 * As the host needs to pre-configure the pVM's vCPU state for
 		 * booting, the protection for pVM is only enforced by the pKVM
@@ -743,6 +744,26 @@ static void pkvm_set_dr7(struct kvm_vcpu *vcpu, unsigned long val)
 		vcpu->arch.switch_db_regs |= KVM_DEBUGREG_BP_ENABLED;
 }
 
+static int pkvm_interrupt_allowed(struct kvm_vcpu *vcpu, bool for_injection)
+{
+	if (!for_injection ||
+	    (!kvm_event_needs_reinjection(vcpu) &&
+	     !vcpu->arch.exception.pending))
+		return kvm_x86_call(interrupt_allowed)(vcpu, for_injection);
+
+	return -EBUSY;
+}
+
+static int pkvm_nmi_allowed(struct kvm_vcpu *vcpu, bool for_injection)
+{
+	if (!for_injection ||
+	    (!kvm_event_needs_reinjection(vcpu) &&
+	     !vcpu->arch.exception.pending))
+		return kvm_x86_call(nmi_allowed)(vcpu, for_injection);
+
+	return -EBUSY;
+}
+
 static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc hc,
 					   union pkvm_hc_data *in, union pkvm_hc_data *out)
 {
@@ -849,6 +870,18 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__enable_irq_window:
 		kvm_x86_call(enable_irq_window)(vcpu);
+		break;
+	case __pkvm__interrupt_allowed:
+		ret = pkvm_interrupt_allowed(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__nmi_allowed:
+		ret = pkvm_nmi_allowed(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__get_nmi_mask:
+		out->get_nmi_mask.data = kvm_x86_call(get_nmi_mask)(vcpu);
+		break;
+	case __pkvm__set_nmi_mask:
+		kvm_x86_call(set_nmi_mask)(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	default:
 		ret = -EINVAL;
