d0947ddde874b40cc8fd9f72bf26a6848e285e69
Chuanxiao Dong <chuanxiao.dong@intel.com>
2025-12-05 15:09:18 +0800

    pKVM: x86: Add post_set_cr3 PV interface
    Add post_set_cr3 PV interface to allow the host to set guest CR3 via the
    vendor-specific post_set_cr3 operation. This PV interface is enforced to
    ensure a pVM's CR3 cannot be changed by the host once the pVM has
    started running.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -583,6 +583,7 @@ static bool is_guest_vcpu_accessible(struct kvm_vcpu *vcpu, enum pkvm_hc hc)
 	case __pkvm__get_msr:
 	case __pkvm__cache_reg:
 	case __pkvm__set_cr4:
+	case __pkvm__post_set_cr3:
 		/*
 		 * As the host needs to pre-configure the pVM's vCPU state for
 		 * booting, the protection for pVM is only enforced by the pKVM
@@ -730,6 +731,9 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		break;
 	case __pkvm__set_cr4:
 		kvm_x86_call(set_cr4)(vcpu, pkvm_hc_input1(hvcpu));
+		break;
+	case __pkvm__post_set_cr3:
+		kvm_x86_call(post_set_cr3)(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	default:
 		ret = -EINVAL;
