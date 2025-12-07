cxdong/pub-pKVM-IA

pKVM: x86: Add lapic instance for the pKVM hypervisor
Add lapic instance, which will be used by the pKVM hypervisor to kick a
remote vcpu out of non-root mode, e.g., for flushing remote TLB.

To simplify, only support the x2apic mode as all the modern CPUs uses
x2apic mode rather than xapic mode at the running time. Add the
dependency with CONFIG_X86_X2APIC to leverage x2apic accessing wrappers.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/lapic.c
@@ -1,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/percpu-defs.h>
+#include <asm/apic.h>
+#include <asm/msr.h>
+#include "pkvm/lapic.h"
+#include "debug.h"
+
+#define LAPIC_MODE_X2APIC (X2APIC_ENABLE | XAPIC_ENABLE)
+
+struct pkvm_lapic {
+	bool ready;
+	u32 apic_id;
+};
+
+static DEFINE_PER_CPU(struct pkvm_lapic, pkvm_lapic);
+
+static int setup_lapic(struct pkvm_lapic *lapic, u64 apicbase)
+{
+	if ((apicbase & LAPIC_MODE_X2APIC) != LAPIC_MODE_X2APIC) {
+		pkvm_err("Lapic is not in X2APIC mode.\n");
+		return -EOPNOTSUPP;
+	}
+
+	lapic->apic_id = native_apic_msr_read(APIC_ID);
+	lapic->ready = true;
+	return 0;
+}
+
+int pkvm_lapic_init(void)
+{
+	struct pkvm_lapic *lapic = this_cpu_ptr(&pkvm_lapic);
+	u64 apicbase;
+
+	if (lapic->ready)
+		return -EBUSY;
+
+	rdmsrl(MSR_IA32_APICBASE, apicbase);
+
+	return setup_lapic(lapic, apicbase);
+}

maluka-dmytro/pKVM-IA

arch/x86/kvm/pkvm/lapic.c
@@ -1,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/percpu-defs.h>
+#include <asm/apic.h>
+#include <asm/msr.h>
+#include "debug.h"
+#include "lapic.h"
+
+#define LAPIC_MODE_X2APIC (X2APIC_ENABLE | XAPIC_ENABLE)
+
+struct pkvm_lapic {
+	bool ready;
+	u32 apic_id;
+};
+
+static DEFINE_PER_CPU(struct pkvm_lapic, pkvm_lapic);
+
+static int setup_lapic(struct pkvm_lapic *lapic, u64 apicbase)
+{
+	if ((apicbase & LAPIC_MODE_X2APIC) != LAPIC_MODE_X2APIC) {
+		pkvm_err("Lapic is not in X2APIC mode.\n");
+		return -EOPNOTSUPP;
+	}
+
+	lapic->apic_id = native_apic_msr_read(APIC_ID);
+	lapic->ready = true;
+	return 0;
+}
+
+int pkvm_lapic_init(void)
+{
+	struct pkvm_lapic *lapic = this_cpu_ptr(&pkvm_lapic);
+	u64 apicbase;
+
+	if (lapic->ready)
+		return -EBUSY;
+
+	rdmsrq(MSR_IA32_APICBASE, apicbase);
+
+	return setup_lapic(lapic, apicbase);
+}