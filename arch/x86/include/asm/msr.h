maluka-dmytro/pKVM-IA

commit 855c8bb91d84077726c2aeac858688dfefd94a6c

pKVM: x86: Add lapic instance for the pKVM hypervisor
Add lapic instance, which will be used by the pKVM hypervisor to kick a
remote vcpu out of non-root mode, e.g., for flushing remote TLB.

To simplify, only support the x2apic mode as all the modern CPUs uses
x2apic mode rather than xapic mode at the running time. Add the
dependency with CONFIG_X86_X2APIC to leverage x2apic accessing wrappers.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/undef.h
@@ -15,5 +15,10 @@
 #undef CONFIG_GENERIC_BUG
 #endif
 #undef CONFIG_CALL_THUNKS_DEBUG
+#undef CONFIG_DEBUG_PREEMPT
+#undef CONFIG_PARAVIRT
+#undef CONFIG_PARAVIRT_XXL
+#undef CONFIG_PARAVIRT_SPINLOCKS
+#undef CONFIG_TRACEPOINTS
 
 #endif /* __PKVM_X86_UNDEF_H */

arch/x86/kvm/Kconfig
@@ -121,6 +121,7 @@ config KVM_INTEL_PROVE_VE
 
 config PKVM_X86
 	bool
+	depends on X86_X2APIC
 
 	help
 	  Provides pKVM x86 generic support.

arch/x86/kvm/pkvm/Makefile
@@ -14,7 +14,7 @@ cmd_deps			:= $(src)/compiling_cmds.sh
 
 pkvm-hyp-y			:= early_alloc.o pkvm.o memory.o cpu.o \
 				   idt.o entry.o init_finalize.o pgtable.o \
-				   mmu.o page_alloc.o
+				   mmu.o page_alloc.o lapic.o
 
 kernel-x86-lib			:= ../../lib
 pkvm-hyp-y			+= $(kernel-x86-lib)/memset_64.o

arch/x86/kvm/pkvm/init_finalize.c
@@ -3,6 +3,7 @@
 #include <asm/kvm_pkvm.h>
 #include "early_alloc.h"
 #include "init_finalize.h"
+#include "lapic.h"
 #include "memory.h"
 #include "mmu.h"
 
@@ -277,6 +278,10 @@ int pkvm_init_finalize(struct pkvm_mem_info infos[], int nr_infos,
 		 */
 		pkvm_hyp_mmu_load();
 	}
+
+	ret = pkvm_lapic_init();
+	if (ret)
+		return ret;
 
 	ret = pkvm_hyp_mmu_finalize(hyp_mmu_finalize_fn);
 	if (ret)

arch/x86/kvm/pkvm/lapic.c
@@ -1,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/percpu-defs.h>
+#include <asm/apic.h>
+#include <asm/msr.h>
+#include "debug.h"
+#include "lapic.h"
+
+#define LAPIC_MODE_X2APIC (X2APIC_ENABLE | XAPIC_ENABLE)
+
+struct pkvm_lapic {
+	bool ready;
+	u32 apic_id;
+};
+
+static DEFINE_PER_CPU(struct pkvm_lapic, pkvm_lapic);
+
+static int setup_lapic(struct pkvm_lapic *lapic, u64 apicbase)
+{
+	if ((apicbase & LAPIC_MODE_X2APIC) != LAPIC_MODE_X2APIC) {
+		pkvm_err("Lapic is not in X2APIC mode.\n");
+		return -EOPNOTSUPP;
+	}
+
+	lapic->apic_id = native_apic_msr_read(APIC_ID);
+	lapic->ready = true;
+	return 0;
+}
+
+int pkvm_lapic_init(void)
+{
+	struct pkvm_lapic *lapic = this_cpu_ptr(&pkvm_lapic);
+	u64 apicbase;
+
+	if (lapic->ready)
+		return -EBUSY;
+
+	rdmsrq(MSR_IA32_APICBASE, apicbase);
+
+	return setup_lapic(lapic, apicbase);
+}

arch/x86/kvm/pkvm/lapic.h
@@ -1,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_X86_LAPIC_H
+#define __PKVM_X86_LAPIC_H
+
+int pkvm_lapic_init(void);
+
+#endif /* __PKVM_X86_LAPIC_H */

commit 69aeb1bd1f2973b62e4766aa972eb3a2c3f9f538

pKVM: x86: Don't undefine CONFIG_TRACEPOINTS
When building kernel with CONFIG_DEBUG_INFO_BTF enabled, pKVM is causing
the following warnings from the resolve_btfids tool:

WARN: multiple IDs found for 'task_struct': 114, 7893 - using 114
WARN: multiple IDs found for 'module': 162, 7963 - using 162
WARN: multiple IDs found for 'file': 726, 7975 - using 726
WARN: multiple IDs found for 'vm_area_struct': 296, 7985 - using 296
WARN: multiple IDs found for 'cgroup': 992, 8045 - using 992
WARN: multiple IDs found for 'inode': 918, 8083 - using 918
WARN: multiple IDs found for 'path': 951, 8113 - using 951
WARN: multiple IDs found for 'seq_file': 1315, 8288 - using 1315
WARN: multiple IDs found for 'bpf_prog': 2008, 8650 - using 2008
WARN: multiple IDs found for 'bpf_map': 2087, 8663 - using 2087
WARN: multiple IDs found for 'bpf_link': 2282, 8777 - using 2282

The root cause is that pKVM disables some CONFIG_xxx options in its
undef.h to be able to include the kernel's headers in the hypervisor
code without the need to modify those headers, and that makes the layout
of the above kernel structures in the hypervisor different from their
layout in the host. Even if that is not harmful for pKVM itself, it at
least may cause CONFIG_DEBUG_INFO_BTF itself to work incorrectly, as it
makes the resolve_btfids tool confused as to which of the multiple
layouts of the same struct to use, when adding the BTF type info to the
kernel image.

CONFIG_TRACEPOINTS is one of such options, so avoid disabling it.
Instead, directly modify the corresponding kernel's header files to use
them in the hypervisor code. Additionally, define NOTRACE to leverage
the fact that include/linux/tracepoint.h respects it, and thus avoid
the need to modify that header as well.

Fixes: 855c8bb91d84 ("pKVM: x86: Add lapic instance for the pKVM hypervisor")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/include/asm/msr.h
@@ -43,7 +43,7 @@ struct saved_msrs {
 #include <asm/atomic.h>
 #include <linux/tracepoint-defs.h>
 
-#ifdef CONFIG_TRACEPOINTS
+#if defined(CONFIG_TRACEPOINTS) && !defined(__PKVM_HYP__)
 DECLARE_TRACEPOINT(read_msr);
 DECLARE_TRACEPOINT(write_msr);
 DECLARE_TRACEPOINT(rdpmc);

arch/x86/kvm/pkvm/undef.h
@@ -21,7 +21,6 @@
 #undef CONFIG_PARAVIRT
 #undef CONFIG_PARAVIRT_XXL
 #undef CONFIG_PARAVIRT_SPINLOCKS
-#undef CONFIG_TRACEPOINTS
 #undef CONFIG_KVM_INTEL_TDX
 #undef CONFIG_PREEMPT_COUNT
 #undef CONFIG_X86_SGX_KVM
@@ -32,5 +31,36 @@
 #undef CONFIG_TRACE_IRQFLAGS
 #undef CONFIG_DEBUG_IRQFLAGS
 #undef CONFIG_DEBUG_PREEMPT
+
+#define NOTRACE
+
+/*
+ * Avoid undefining the below options, as it would change the layout of some
+ * of the kernel's data structures. Most importantly this applies to structs
+ * that are shared between the host kernel and pKVM, e.g. struct kvm and
+ * struct kvm_vcpu. They absolutely must have the same size and layout in the
+ * host KVM and in pKVM. Among the below options, CONFIG_LOCKDEP is known to
+ * affect their layout.
+ *
+ * But even for structs that are not shared between host and pKVM, such as
+ * task_struct or vm_area_struct, it is still better to avoid changing their
+ * layout. For instance, with CONFIG_DEBUG_INFO_BTF enabled, if those structs
+ * are different in the host and in pKVM, multiple BTF type IDs are generated
+ * for them, and then the resolve_btfids tool warns about multiple IDs found
+ * and uses just one of them when adding the needed debug info to the vmlinux,
+ * and there is no guarantee that it will use the correct type ID, i.e. the
+ * host's one, not the pKVM's one.
+ *
+ * The following can be used (with CONFIG_DEBUG_INFO_BTF=y) to inspect layouts
+ * of structs in the kernel image, for checking if there are no multiple
+ * layouts of the same struct caused by pKVM mangling the config options:
+ *
+ *     make -C tools/bpf/bpftool
+ *     tools/bpf/bpftool/bpftool btf dump file vmlinux.unstripped
+ */
+/*
+#undef CONFIG_LOCKDEP
+#undef CONFIG_TRACEPOINTS
+*/
 
 #endif /* __PKVM_X86_UNDEF_H */

include/linux/tracepoint-defs.h
@@ -87,7 +87,7 @@ struct bpf_raw_event_map {
 #define DECLARE_TRACEPOINT(tp) \
 	extern struct tracepoint __tracepoint_##tp
 
-#ifdef CONFIG_TRACEPOINTS
+#if defined(CONFIG_TRACEPOINTS) && !defined(__PKVM_HYP__)
 # define tracepoint_enabled(tp) \
 	static_branch_unlikely(&(__tracepoint_##tp).key)
 #else
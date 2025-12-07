69aeb1bd1f2973b62e4766aa972eb3a2c3f9f538
Dmytro Maluka <dmaluka@google.com>
2025-12-07 16:24:08 +0000

    pKVM: x86: Don't undefine CONFIG_TRACEPOINTS
    When building kernel with CONFIG_DEBUG_INFO_BTF enabled, pKVM is causing
    the following warnings from the resolve_btfids tool:
    
    WARN: multiple IDs found for 'task_struct': 114, 7893 - using 114
    WARN: multiple IDs found for 'module': 162, 7963 - using 162
    WARN: multiple IDs found for 'file': 726, 7975 - using 726
    WARN: multiple IDs found for 'vm_area_struct': 296, 7985 - using 296
    WARN: multiple IDs found for 'cgroup': 992, 8045 - using 992
    WARN: multiple IDs found for 'inode': 918, 8083 - using 918
    WARN: multiple IDs found for 'path': 951, 8113 - using 951
    WARN: multiple IDs found for 'seq_file': 1315, 8288 - using 1315
    WARN: multiple IDs found for 'bpf_prog': 2008, 8650 - using 2008
    WARN: multiple IDs found for 'bpf_map': 2087, 8663 - using 2087
    WARN: multiple IDs found for 'bpf_link': 2282, 8777 - using 2282
    
    The root cause is that pKVM disables some CONFIG_xxx options in its
    undef.h to be able to include the kernel's headers in the hypervisor
    code without the need to modify those headers, and that makes the layout
    of the above kernel structures in the hypervisor different from their
    layout in the host. Even if that is not harmful for pKVM itself, it at
    least may cause CONFIG_DEBUG_INFO_BTF itself to work incorrectly, as it
    makes the resolve_btfids tool confused as to which of the multiple
    layouts of the same struct to use, when adding the BTF type info to the
    kernel image.
    
    CONFIG_TRACEPOINTS is one of such options, so avoid disabling it.
    Instead, directly modify the corresponding kernel's header files to use
    them in the hypervisor code. Additionally, define NOTRACE to leverage
    the fact that include/linux/tracepoint.h respects it, and thus avoid
    the need to modify that header as well.
    
    Fixes: 855c8bb91d84 ("pKVM: x86: Add lapic instance for the pKVM hypervisor")
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/undef.h
+++ arch/x86/kvm/pkvm/undef.h
@@ -21,7 +21,6 @@
 #undef CONFIG_PARAVIRT
 #undef CONFIG_PARAVIRT_XXL
 #undef CONFIG_PARAVIRT_SPINLOCKS
-#undef CONFIG_TRACEPOINTS
 #undef CONFIG_KVM_INTEL_TDX
 #undef CONFIG_PREEMPT_COUNT
 #undef CONFIG_X86_SGX_KVM
@@ -32,5 +31,36 @@
 #undef CONFIG_TRACE_IRQFLAGS
 #undef CONFIG_DEBUG_IRQFLAGS
 #undef CONFIG_DEBUG_PREEMPT
+
+#define NOTRACE
+
+/*
+ * Avoid undefining the below options, as it would change the layout of some
+ * of the kernel's data structures. Most importantly this applies to structs
+ * that are shared between the host kernel and pKVM, e.g. struct kvm and
+ * struct kvm_vcpu. They absolutely must have the same size and layout in the
+ * host KVM and in pKVM. Among the below options, CONFIG_LOCKDEP is known to
+ * affect their layout.
+ *
+ * But even for structs that are not shared between host and pKVM, such as
+ * task_struct or vm_area_struct, it is still better to avoid changing their
+ * layout. For instance, with CONFIG_DEBUG_INFO_BTF enabled, if those structs
+ * are different in the host and in pKVM, multiple BTF type IDs are generated
+ * for them, and then the resolve_btfids tool warns about multiple IDs found
+ * and uses just one of them when adding the needed debug info to the vmlinux,
+ * and there is no guarantee that it will use the correct type ID, i.e. the
+ * host's one, not the pKVM's one.
+ *
+ * The following can be used (with CONFIG_DEBUG_INFO_BTF=y) to inspect layouts
+ * of structs in the kernel image, for checking if there are no multiple
+ * layouts of the same struct caused by pKVM mangling the config options:
+ *
+ *     make -C tools/bpf/bpftool
+ *     tools/bpf/bpftool/bpftool btf dump file vmlinux.unstripped
+ */
+/*
+#undef CONFIG_LOCKDEP
+#undef CONFIG_TRACEPOINTS
+*/
 
 #endif /* __PKVM_X86_UNDEF_H */
