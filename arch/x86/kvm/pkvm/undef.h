maluka-dmytro/pKVM-IA

commit a47595e9a2deaf12624cb6120f970316f2e2ef39

pKVM: x86: Redefine KVM print macros for the pKVM hypervisor
The KVM print macros may be included in the code which are leveraged
by the pKVM hypervisor. Overridden these print macros to use the "pkvm"
prefixes, and avoid printing current task information which is not
supported in the pKVM hypervisor.

Disable CONFIG_DYNAMIC_DEBUG and CONFIG_DYNAMIC_DEBUG_CORE to strip off
the unsupported print symbols for pKVM non-debug version.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/undef.h
@@ -13,6 +13,8 @@
 #undef CONFIG_PRINTK
 #undef CONFIG_BUG
 #undef CONFIG_GENERIC_BUG
+#undef CONFIG_DYNAMIC_DEBUG
+#undef CONFIG_DYNAMIC_DEBUG_CORE
 #endif
 #undef CONFIG_CALL_THUNKS_DEBUG
 #undef CONFIG_DEBUG_PREEMPT

include/linux/kvm_host.h
@@ -878,6 +878,18 @@ struct kvm {
 	char stats_id[KVM_STATS_NAME_SIZE];
 };
 
+#ifdef __PKVM_HYP__
+#define kvm_err(fmt, ...) \
+	pr_err("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_info(fmt, ...) \
+	pr_info("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_debug(fmt, ...) \
+	pr_debug("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_debug_ratelimited(fmt, ...) \
+	pr_debug_ratelimited("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_pr_unimpl(fmt, ...) \
+	pr_err_ratelimited("pkvm: " fmt, ## __VA_ARGS__)
+#else
 #define kvm_err(fmt, ...) \
 	pr_err("kvm [%i]: " fmt, task_pid_nr(current), ## __VA_ARGS__)
 #define kvm_info(fmt, ...) \
@@ -890,6 +902,7 @@ struct kvm {
 #define kvm_pr_unimpl(fmt, ...) \
 	pr_err_ratelimited("kvm [%i]: " fmt, \
 			   task_tgid_nr(current), ## __VA_ARGS__)
+#endif
 
 /* The guest did something we don't support. */
 #define vcpu_unimpl(vcpu, fmt, ...)					\

commit 997e0a740a8d56ca07140494aa19664f59ea9400

pKVM: x86: Don't undefine CONFIG_DYNAMIC_DEBUG
For the same reasons as in the previous 2 commits, avoid undefining
CONFIG_DYNAMIC_DEBUG and CONFIG_DYNAMIC_DEBUG_CORE, and instead directly
modify the corresponding kernel's headers to use them in the hypervisor
code.

Fixes: a47595e9a2de ("pKVM: x86: Redefine KVM print macros for the pKVM hypervisor")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/undef.h
@@ -11,8 +11,6 @@
  */
 #ifndef CONFIG_PKVM_X86_DEBUG
 #undef CONFIG_PRINTK
-#undef CONFIG_DYNAMIC_DEBUG
-#undef CONFIG_DYNAMIC_DEBUG_CORE
 #endif
 #undef CONFIG_CALL_THUNKS_DEBUG
 #undef CONFIG_DEBUG_PREEMPT
@@ -60,6 +58,8 @@
 #ifndef CONFIG_PKVM_X86_DEBUG
 #undef CONFIG_BUG
 #undef CONFIG_GENERIC_BUG
+#undef CONFIG_DYNAMIC_DEBUG
+#undef CONFIG_DYNAMIC_DEBUG_CORE
 #endif
 #undef CONFIG_LOCKDEP
 #undef CONFIG_TRACEPOINTS

include/linux/dynamic_debug.h
@@ -134,8 +134,9 @@ struct ddebug_class_param {
  * pr_debug() and friends are globally enabled or modules have selectively
  * enabled them.
  */
-#if defined(CONFIG_DYNAMIC_DEBUG) || \
-	(defined(CONFIG_DYNAMIC_DEBUG_CORE) && defined(DYNAMIC_DEBUG_MODULE))
+#if (defined(CONFIG_DYNAMIC_DEBUG) || \
+	(defined(CONFIG_DYNAMIC_DEBUG_CORE) && defined(DYNAMIC_DEBUG_MODULE))) && \
+	(!defined(__PKVM_HYP__) || defined(CONFIG_PKVM_X86_DEBUG))
 
 extern __printf(2, 3)
 void __dynamic_pr_debug(struct _ddebug *descriptor, const char *fmt, ...);

include/linux/printk.h
@@ -735,8 +735,9 @@ struct pi_entry {
 #endif
 
 /* If you are writing a driver, please use dev_dbg instead */
-#if defined(CONFIG_DYNAMIC_DEBUG) || \
-	(defined(CONFIG_DYNAMIC_DEBUG_CORE) && defined(DYNAMIC_DEBUG_MODULE))
+#if (defined(CONFIG_DYNAMIC_DEBUG) || \
+	(defined(CONFIG_DYNAMIC_DEBUG_CORE) && defined(DYNAMIC_DEBUG_MODULE))) && \
+	(!defined(__PKVM_HYP__) || defined(CONFIG_PKVM_X86_DEBUG))
 /* descriptor check is first to prevent flooding with "callbacks suppressed" */
 #define pr_debug_ratelimited(fmt, ...)					\
 do {									\