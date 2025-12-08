maluka-dmytro/pKVM-IA

commit 855c8bb91d84077726c2aeac858688dfefd94a6c

pKVM: x86: Add lapic instance for the pKVM hypervisor
Add lapic instance, which will be used by the pKVM hypervisor to kick a
remote vcpu out of non-root mode, e.g., for flushing remote TLB.

To simplify, only support the x2apic mode as all the modern CPUs uses
x2apic mode rather than xapic mode at the running time. Add the
dependency with CONFIG_X86_X2APIC to leverage x2apic accessing wrappers.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/undef.h
@@ -15,5 +15,10 @@
 #undef CONFIG_GENERIC_BUG
 #endif
 #undef CONFIG_CALL_THUNKS_DEBUG
+#undef CONFIG_DEBUG_PREEMPT
+#undef CONFIG_PARAVIRT
+#undef CONFIG_PARAVIRT_XXL
+#undef CONFIG_PARAVIRT_SPINLOCKS
+#undef CONFIG_TRACEPOINTS
 
 #endif /* __PKVM_X86_UNDEF_H */

arch/x86/kvm/Kconfig
@@ -121,6 +121,7 @@ config KVM_INTEL_PROVE_VE
 
 config PKVM_X86
 	bool
+	depends on X86_X2APIC
 
 	help
 	  Provides pKVM x86 generic support.

arch/x86/kvm/pkvm/Makefile
@@ -14,7 +14,7 @@ cmd_deps			:= $(src)/compiling_cmds.sh
 
 pkvm-hyp-y			:= early_alloc.o pkvm.o memory.o cpu.o \
 				   idt.o entry.o init_finalize.o pgtable.o \
-				   mmu.o page_alloc.o
+				   mmu.o page_alloc.o lapic.o
 
 kernel-x86-lib			:= ../../lib
 pkvm-hyp-y			+= $(kernel-x86-lib)/memset_64.o

arch/x86/kvm/pkvm/init_finalize.c
@@ -3,6 +3,7 @@
 #include <asm/kvm_pkvm.h>
 #include "early_alloc.h"
 #include "init_finalize.h"
+#include "lapic.h"
 #include "memory.h"
 #include "mmu.h"
 
@@ -277,6 +278,10 @@ int pkvm_init_finalize(struct pkvm_mem_info infos[], int nr_infos,
 		 */
 		pkvm_hyp_mmu_load();
 	}
+
+	ret = pkvm_lapic_init();
+	if (ret)
+		return ret;
 
 	ret = pkvm_hyp_mmu_finalize(hyp_mmu_finalize_fn);
 	if (ret)

arch/x86/kvm/pkvm/lapic.c
@@ -1,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/percpu-defs.h>
+#include <asm/apic.h>
+#include <asm/msr.h>
+#include "debug.h"
+#include "lapic.h"
+
+#define LAPIC_MODE_X2APIC (X2APIC_ENABLE | XAPIC_ENABLE)
+
+struct pkvm_lapic {
+	bool ready;
+	u32 apic_id;
+};
+
+static DEFINE_PER_CPU(struct pkvm_lapic, pkvm_lapic);
+
+static int setup_lapic(struct pkvm_lapic *lapic, u64 apicbase)
+{
+	if ((apicbase & LAPIC_MODE_X2APIC) != LAPIC_MODE_X2APIC) {
+		pkvm_err("Lapic is not in X2APIC mode.\n");
+		return -EOPNOTSUPP;
+	}
+
+	lapic->apic_id = native_apic_msr_read(APIC_ID);
+	lapic->ready = true;
+	return 0;
+}
+
+int pkvm_lapic_init(void)
+{
+	struct pkvm_lapic *lapic = this_cpu_ptr(&pkvm_lapic);
+	u64 apicbase;
+
+	if (lapic->ready)
+		return -EBUSY;
+
+	rdmsrq(MSR_IA32_APICBASE, apicbase);
+
+	return setup_lapic(lapic, apicbase);
+}

arch/x86/kvm/pkvm/lapic.h
@@ -1,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __PKVM_X86_LAPIC_H
+#define __PKVM_X86_LAPIC_H
+
+int pkvm_lapic_init(void);
+
+#endif /* __PKVM_X86_LAPIC_H */

commit 0a466ec5e43714b984d2ae767321b9d7f311ec4a

pKVM: x86: Don't undefine CONFIG_DEBUG_PREEMPT
For the same reasons as in the previous commits, avoid undefining
CONFIG_DEBUG_PREEMPT, and instead directly modify the corresponding
kernel's headers to use them in the hypervisor code.

Fixes: 855c8bb91d84 ("pKVM: x86: Add lapic instance for the pKVM hypervisor")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/undef.h
@@ -13,7 +13,6 @@
 #undef CONFIG_PRINTK
 #endif
 #undef CONFIG_CALL_THUNKS_DEBUG
-#undef CONFIG_DEBUG_PREEMPT
 #undef CONFIG_PARAVIRT
 #undef CONFIG_PARAVIRT_XXL
 #undef CONFIG_PARAVIRT_SPINLOCKS
@@ -25,7 +24,6 @@
 #undef CONFIG_X86_DEBUG_FPU
 #undef CONFIG_PROVE_LOCKING
 #undef CONFIG_DEBUG_IRQFLAGS
-#undef CONFIG_DEBUG_PREEMPT
 
 #define NOTRACE
 
@@ -63,6 +61,7 @@
 #undef CONFIG_LOCKDEP
 #undef CONFIG_TRACEPOINTS
 #undef CONFIG_TRACE_IRQFLAGS
+#undef CONFIG_DEBUG_PREEMPT
 */
 
 #endif /* __PKVM_X86_UNDEF_H */

include/linux/percpu-defs.h
@@ -311,7 +311,7 @@ do {									\
 
 extern void __bad_size_call_parameter(void);
 
-#ifdef CONFIG_DEBUG_PREEMPT
+#if defined(CONFIG_DEBUG_PREEMPT) && !defined(__PKVM_HYP__)
 extern void __this_cpu_preempt_check(const char *op);
 #else
 static __always_inline void __this_cpu_preempt_check(const char *op) { }

include/linux/smp.h
@@ -265,7 +265,7 @@ static inline int get_boot_cpu_id(void)
 #define __smp_processor_id() raw_smp_processor_id()
 #endif
 
-#ifdef CONFIG_DEBUG_PREEMPT
+#if defined(CONFIG_DEBUG_PREEMPT) && !defined(__PKVM_HYP__)
   extern unsigned int debug_smp_processor_id(void);
 # define smp_processor_id() debug_smp_processor_id()
 #else