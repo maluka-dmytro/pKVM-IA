maluka-dmytro/pKVM-IA

commit 1695301e8fe6d1ad2ff76a971b81acbfedb47d94

pKVM: VMX: Implement set_msr operation
Import vmx_set_msr() from KVM for pKVM to implement set_msr operation
with some unsupported MSRs being disabled.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/undef.h
@@ -29,5 +29,7 @@
 #undef CONFIG_MATH_EMULATION
 #undef CONFIG_X86_DEBUG_FPU
 #undef CONFIG_PROVE_LOCKING
+#undef CONFIG_TRACE_IRQFLAGS
+#undef CONFIG_DEBUG_IRQFLAGS
 
 #endif /* __PKVM_X86_UNDEF_H */

arch/x86/kvm/vmx/main.c
@@ -900,8 +900,10 @@ struct kvm_x86_ops vt_x86_ops __initdata = {
 #ifndef __PKVM_HYP__
 	.get_feature_msr = vmx_get_feature_msr,
 	.get_msr = vt_op(get_msr),
+#endif
 	.set_msr = vt_op(set_msr),
 
+#ifndef __PKVM_HYP__
 	.get_segment_base = vt_op(get_segment_base),
 	.get_segment = vt_op(get_segment),
 	.set_segment = vt_op(set_segment),

arch/x86/kvm/vmx/nested.h
@@ -8,8 +8,11 @@
 #include "vmx.h"
 
 #ifdef __PKVM_HYP__
+static inline void vmx_leave_nested(struct kvm_vcpu *vcpu) {}
 static inline void nested_vmx_setup_ctls_msrs(struct vmcs_config *vmcs_conf, u32 ept_caps) {}
 static inline void nested_vmx_free_vcpu(struct kvm_vcpu *vcpu) {}
+static inline void nested_vmx_set_vmcs_shadowing_bitmap(void) {}
+static inline int vmx_set_vmx_msr(struct kvm_vcpu *vcpu, u32 msr_index, u64 data) { return 1; }
 
 static inline struct vmcs12 *get_vmcs12(struct kvm_vcpu *vcpu)
 {

arch/x86/kvm/vmx/vmx.c
@@ -394,6 +394,7 @@ static __always_inline void vmx_enable_fb_clear(struct vcpu_vmx *vmx)
 	vmx->msr_ia32_mcu_opt_ctrl &= ~FB_CLEAR_DIS;
 	native_wrmsrq(MSR_IA32_MCU_OPT_CTRL, vmx->msr_ia32_mcu_opt_ctrl);
 }
+#endif /* !__PKVM_HYP__ */
 
 static void vmx_update_fb_clear_dis(struct kvm_vcpu *vcpu, struct vcpu_vmx *vmx)
 {
@@ -424,6 +425,7 @@ static void vmx_update_fb_clear_dis(struct kvm_vcpu *vcpu, struct vcpu_vmx *vmx)
 		vmx->disable_fb_clear = false;
 }
 
+#ifndef __PKVM_HYP__
 static const struct kernel_param_ops vmentry_l1d_flush_ops = {
 	.set = vmentry_l1d_flush_set,
 	.get = vmentry_l1d_flush_get,
@@ -693,14 +695,21 @@ struct vmx_uret_msr *vmx_find_uret_msr(struct vcpu_vmx *vmx, u32 msr)
 	return NULL;
 }
 
-#ifndef __PKVM_HYP__
 static int vmx_set_guest_uret_msr(struct vcpu_vmx *vmx,
 				  struct vmx_uret_msr *msr, u64 data)
 {
 	unsigned int slot = msr - vmx->guest_uret_msrs;
 	int ret = 0;
 
+#ifndef __PKVM_HYP__
 	if (msr->load_into_hardware) {
+#else
+	/*
+	 * The host may use set_msr PV interface to access uret MSRs and in this
+	 * case, the uret MSRs are not loaded to the hardware.
+	 */
+	if (msr->load_into_hardware && vmx->guest_uret_msrs_loaded) {
+#endif
 		preempt_disable();
 		ret = kvm_set_user_return_msr(slot, data, msr->mask);
 		preempt_enable();
@@ -710,6 +719,7 @@ static int vmx_set_guest_uret_msr(struct vcpu_vmx *vmx,
 	return ret;
 }
 
+#ifndef __PKVM_HYP__
 /*
  * Disable VMX and clear CR4.VMXE (even if VMXOFF faults)
  *
@@ -1385,8 +1395,10 @@ static void vmx_prepare_switch_to_host(struct vcpu_vmx *vmx)
 	vmx->vt.guest_state_loaded = false;
 	vmx->guest_uret_msrs_loaded = false;
 }
+#endif /* !__PKVM_HYP__ */
 
 #ifdef CONFIG_X86_64
+#ifndef __PKVM_HYP__
 static u64 vmx_read_guest_host_msr(struct vcpu_vmx *vmx, u32 msr, u64 *cache)
 {
 	preempt_disable();
@@ -1395,6 +1407,7 @@ static u64 vmx_read_guest_host_msr(struct vcpu_vmx *vmx, u32 msr, u64 *cache)
 	preempt_enable();
 	return *cache;
 }
+#endif /* !__PKVM_HYP__ */
 
 static void vmx_write_guest_host_msr(struct vcpu_vmx *vmx, u32 msr, u64 data,
 				     u64 *cache)
@@ -1406,11 +1419,13 @@ static void vmx_write_guest_host_msr(struct vcpu_vmx *vmx, u32 msr, u64 data,
 	*cache = data;
 }
 
+#ifndef __PKVM_HYP__
 static u64 vmx_read_guest_kernel_gs_base(struct vcpu_vmx *vmx)
 {
 	return vmx_read_guest_host_msr(vmx, MSR_KERNEL_GS_BASE,
 				       &vmx->msr_guest_kernel_gs_base);
 }
+#endif /* !__PKVM_HYP__ */
 
 static void vmx_write_guest_kernel_gs_base(struct vcpu_vmx *vmx, u64 data)
 {
@@ -1419,6 +1434,7 @@ static void vmx_write_guest_kernel_gs_base(struct vcpu_vmx *vmx, u64 data)
 }
 #endif
 
+#ifndef __PKVM_HYP__
 static void grow_ple_window(struct kvm_vcpu *vcpu)
 {
 	struct vcpu_vmx *vmx = to_vmx(vcpu);
@@ -2019,6 +2035,7 @@ void vmx_write_tsc_multiplier(struct kvm_vcpu *vcpu)
 {
 	vmcs_write64(TSC_MULTIPLIER, vcpu->arch.tsc_scaling_ratio);
 }
+#endif /* !__PKVM_HYP__ */
 
 /*
  * Userspace is allowed to set any supported IA32_FEATURE_CONTROL regardless of
@@ -2058,6 +2075,7 @@ static inline bool is_vmx_feature_control_msr_valid(struct vcpu_vmx *vmx,
 	return !(msr->data & ~valid_bits);
 }
 
+#ifndef __PKVM_HYP__
 int vmx_get_feature_msr(u32 msr, u64 *data)
 {
 	switch (msr) {
@@ -2234,6 +2252,7 @@ int vmx_get_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 
 	return 0;
 }
+#endif /* !__PKVM_HYP__ */
 
 static u64 nested_vmx_truncate_sysenter_addr(struct kvm_vcpu *vcpu,
 						    u64 data)
@@ -2253,9 +2272,11 @@ u64 vmx_get_supported_debugctl(struct kvm_vcpu *vcpu, bool host_initiated)
 	    (host_initiated || guest_cpu_cap_has(vcpu, X86_FEATURE_BUS_LOCK_DETECT)))
 		debugctl |= DEBUGCTLMSR_BUS_LOCK_DETECT;
 
+#ifndef __PKVM_HYP__
 	if ((kvm_caps.supported_perf_cap & PERF_CAP_LBR_FMT) &&
 	    (host_initiated || intel_pmu_lbr_is_enabled(vcpu)))
 		debugctl |= DEBUGCTLMSR_LBR | DEBUGCTLMSR_FREEZE_LBRS_ON_PMI;
+#endif
 
 	if (boot_cpu_has(X86_FEATURE_RTM) &&
 	    (host_initiated || guest_cpu_cap_has(vcpu, X86_FEATURE_RTM)))
@@ -2288,7 +2309,9 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 	int ret = 0;
 	u32 msr_index = msr_info->index;
 	u64 data = msr_info->data;
+#ifndef __PKVM_HYP__
 	u32 index;
+#endif
 
 	switch (msr_index) {
 	case MSR_EFER:
@@ -2356,9 +2379,15 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 
 		vmx_guest_debugctl_write(vcpu, data);
 
+		/*
+		 * The pKVM doesn't support guest PMU emulation. Disabling this
+		 * code to avoid importing unnecessary symbols.
+		 */
+#ifndef __PKVM_HYP__
 		if (intel_pmu_lbr_is_enabled(vcpu) && !to_vmx(vcpu)->lbr_desc.event &&
 		    (data & DEBUGCTLMSR_LBR))
 			intel_pmu_create_guest_lbr_event(vcpu);
+#endif
 		return 0;
 	case MSR_IA32_BNDCFGS:
 		if (!kvm_mpx_supported() ||
@@ -2478,6 +2507,9 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		if (!guest_cpu_cap_has(vcpu, X86_FEATURE_VMX))
 			return 1;
 		return vmx_set_vmx_msr(vcpu, msr_index, data);
+
+	/* The pKVM hypervisor doesn't support PT guest mode. */
+#ifndef __PKVM_HYP__
 	case MSR_IA32_RTIT_CTL:
 		if (!vmx_pt_mode_is_host_guest() ||
 			vmx_rtit_ctl_check(vcpu, data) ||
@@ -2537,6 +2569,17 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		else
 			vmx->pt_desc.guest.addr_a[index / 2] = data;
 		break;
+#else
+	/* The pKVM hypervisor doesn't support PT guest mode. */
+	case MSR_IA32_RTIT_CTL:
+	case MSR_IA32_RTIT_STATUS:
+	case MSR_IA32_RTIT_CR3_MATCH:
+	case MSR_IA32_RTIT_OUTPUT_BASE:
+	case MSR_IA32_RTIT_OUTPUT_MASK:
+	case MSR_IA32_RTIT_ADDR0_A ... MSR_IA32_RTIT_ADDR3_B:
+		WARN_ON_ONCE(vmx_pt_mode_is_host_guest());
+		return 1;
+#endif
 	case MSR_IA32_S_CET:
 		vmcs_writel(GUEST_S_CET, data);
 		break;
@@ -2547,6 +2590,7 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		vmcs_writel(GUEST_INTR_SSP_TABLE, data);
 		break;
 	case MSR_IA32_PERF_CAPABILITIES:
+#ifndef __PKVM_HYP__
 		if (data & PERF_CAP_LBR_FMT) {
 			if ((data & PERF_CAP_LBR_FMT) !=
 			    (kvm_caps.supported_perf_cap & PERF_CAP_LBR_FMT))
@@ -2567,7 +2611,13 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		}
 		ret = kvm_set_msr_common(vcpu, msr_info);
 		break;
-
+#else
+		/*
+		 * The pKVM hypervisor doesn't support emulating PMU for guest
+		 * thus also the IA32_PER_CAPABILITIES.
+		 */
+		return 1;
+#endif
 	default:
 	find_uret_msr:
 		msr = vmx_find_uret_msr(vmx, msr_index);
@@ -2584,6 +2634,7 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 	return ret;
 }
 
+#ifndef __PKVM_HYP__
 void vmx_cache_reg(struct kvm_vcpu *vcpu, enum kvm_reg reg)
 {
 	unsigned long guest_owned_bits;

arch/x86/kvm/x86.c
@@ -668,14 +668,15 @@ void kvm_user_return_msr_cpu_online(void)
 	}
 }
 
-#ifndef __PKVM_HYP__
 static void kvm_user_return_register_notifier(struct kvm_user_return_msrs *msrs)
 {
+#ifndef __PKVM_HYP__
 	if (!msrs->registered) {
 		msrs->urn.on_user_return = kvm_on_user_return;
 		user_return_notifier_register(&msrs->urn);
 		msrs->registered = true;
 	}
+#endif
 }
 
 int kvm_set_user_return_msr(unsigned slot, u64 value, u64 mask)
@@ -696,6 +697,7 @@ int kvm_set_user_return_msr(unsigned slot, u64 value, u64 mask)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_set_user_return_msr);
 
+#ifndef __PKVM_HYP__
 void kvm_user_return_msr_update_cache(unsigned int slot, u64 value)
 {
 	struct kvm_user_return_msrs *msrs = this_cpu_ptr(user_return_msrs);
@@ -14144,6 +14146,7 @@ void kvm_arch_gmem_invalidate(kvm_pfn_t start, kvm_pfn_t end)
 }
 #endif
 #endif
+#endif /* !__PKVM_HYP__ */
 
 int kvm_spec_ctrl_test_value(u64 value)
 {
@@ -14171,6 +14174,7 @@ int kvm_spec_ctrl_test_value(u64 value)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_spec_ctrl_test_value);
 
+#ifndef __PKVM_HYP__
 void kvm_fixup_and_inject_pf_error(struct kvm_vcpu *vcpu, gva_t gva, u16 error_code)
 {
 	struct kvm_mmu *mmu = vcpu->arch.walk_mmu;

commit 5e9bb82e23dd197c60af9e30fd1a80d650f4322b

pKVM: x86: Don't undefine CONFIG_TRACE_IRQFLAGS
For the same reasons as in the previous commits, avoid undefining
CONFIG_TRACE_IRQFLAGS, and instead directly modify the corresponding
kernel's headers to use them in the hypervisor code.

Fixes: 1695301e8fe6 ("pKVM: VMX: Implement set_msr operation")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/undef.h
@@ -24,7 +24,6 @@
 #undef CONFIG_MATH_EMULATION
 #undef CONFIG_X86_DEBUG_FPU
 #undef CONFIG_PROVE_LOCKING
-#undef CONFIG_TRACE_IRQFLAGS
 #undef CONFIG_DEBUG_IRQFLAGS
 #undef CONFIG_DEBUG_PREEMPT
 
@@ -63,6 +62,7 @@
 #endif
 #undef CONFIG_LOCKDEP
 #undef CONFIG_TRACEPOINTS
+#undef CONFIG_TRACE_IRQFLAGS
 */
 
 #endif /* __PKVM_X86_UNDEF_H */

include/linux/irqflags.h
@@ -39,7 +39,7 @@ struct task_struct;
 					      struct task_struct *idle) {}
 #endif
 
-#ifdef CONFIG_TRACE_IRQFLAGS
+#if defined(CONFIG_TRACE_IRQFLAGS) && !defined(__PKVM_HYP__)
 
 DECLARE_PER_CPU(int, hardirqs_enabled);
 DECLARE_PER_CPU(int, hardirq_context);
@@ -127,7 +127,7 @@ do {						\
 # define lockdep_irq_work_exit(__work)		do { } while (0)
 #endif
 
-#if defined(CONFIG_TRACE_IRQFLAGS) && !defined(CONFIG_PREEMPT_RT)
+#if defined(CONFIG_TRACE_IRQFLAGS) && !defined(CONFIG_PREEMPT_RT) && !defined(__PKVM_HYP__)
 # define lockdep_softirq_enter()		\
 do {						\
 	current->softirq_context++;		\