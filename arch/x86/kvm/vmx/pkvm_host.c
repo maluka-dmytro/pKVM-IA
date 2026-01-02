6bda85015ef99df197f8c88f962296709a4eaf72
Dmytro Maluka <dmaluka@google.com>
2026-01-02 21:55:49 +0000

    KVM: x86/mmu: pKVM: Track guest mappings in an interval tree
    Similarly to pKVM-ARM, let the host remember mapped guest pages in an
    interval tree, to use this information afterwards in various cases (e.g.
    for detecting spurious page faults, or for unmapping mapped pages within
    the given range when handling MMU invalidate notifiers).
    
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/vmx/pkvm_host.c
+++ arch/x86/kvm/vmx/pkvm_host.c
@@ -713,6 +713,7 @@ free_page:
 static void pkvm_vm_destroy(struct kvm *kvm)
 {
 	int vm_handle = kvm->arch.pkvm.handle;
+	struct pkvm_mapping *mapping;
 	union pkvm_hc_data out;
 	int ret;
 
@@ -724,6 +725,16 @@ static void pkvm_vm_destroy(struct kvm *kvm)
 
 	kvm_free_pkvm_memcache(&out.vm_destroy.memcache);
 	kvm_free_pkvm_memcache(&kvm->arch.pkvm.guest_mmu_teardown_mc);
+
+	/*
+	 * It would be nice to do this in generic code in kvm_mmu_uninit_vm(),
+	 * not here. But kvm_mmu_uninit_vm() is called too early, when pKVM
+	 * has not released guest pages to the host yet.
+	 */
+	for_each_pkvm_mapping(kvm, 0, U64_MAX, mapping) {
+		pkvm_mapping_remove(mapping, &kvm->arch.pkvm.mappings);
+		kfree(mapping);
+	}
 
 	vmx_vm_destroy(kvm);
 }
