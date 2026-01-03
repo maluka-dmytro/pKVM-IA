a8785241b6a6c6aac8ab6102bc635eb0d0d8e02e
Dmytro Maluka <dmaluka@google.com>
2026-01-03 17:53:06 +0000

    WIP: pKVM: x86: Remove unused load_mmu_pgd arguments
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -1052,7 +1052,7 @@ static void pkvm_write_tsc_multiplier(struct kvm_vcpu *vcpu, u64 ratio)
 	kvm_x86_call(write_tsc_multiplier)(vcpu);
 }
 
-static void pkvm_load_mmu_pgd(struct kvm_vcpu *vcpu, hpa_t root_hpa, int root_level)
+static void pkvm_load_mmu_pgd(struct kvm_vcpu *vcpu)
 {
 	kvm_x86_call(load_mmu_pgd)(vcpu, vcpu->arch.mmu->root.hpa,
 				   vcpu->arch.mmu->root_role.level);
@@ -1480,7 +1480,7 @@ static int pkvm_vcpu_handle_host_hypercall(struct kvm_vcpu *hvcpu, enum pkvm_hc 
 		pkvm_write_tsc_multiplier(vcpu, pkvm_hc_input1(hvcpu));
 		break;
 	case __pkvm__load_mmu_pgd:
-		pkvm_load_mmu_pgd(vcpu, pkvm_hc_input1(hvcpu), pkvm_hc_input2(hvcpu));
+		pkvm_load_mmu_pgd(vcpu);
 		break;
 	case __pkvm__setup_mce:
 		ret = kvm_vcpu_x86_setup_mce(vcpu, pkvm_hc_input1(hvcpu));
