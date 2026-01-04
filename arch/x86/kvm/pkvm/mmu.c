cxdong/pub-pKVM-IA

commit ca66f34a8120de466f12e06c77e51126c89b0fd0

pKVM: x86: Switch to use buddy allocator for the hyp mmu
The hyp mmu was initialized by using the early allocator. After the
pkvm_vmemmap is backed in the hyp mmu, the buddy allocator is ready to
use. So load the pKVM hypervisor's own mmu, and re-initialize the hyp
mmu page table with using buddy allocator.

Since some of hyp mmu pages were allocated by the early allocator which
is not able to initialize the page refcount stored in the pkvm_page
structure, fix this by walking the hyp mmu page table and initialize
the page refcount for each present page table pages.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/mmu.c
@@ -3,10 +3,39 @@
 #include <linux/pgtable.h>
 #include <asm/kvm_pkvm.h>
 #include "early_alloc.h"
+#include "gfp.h"
 #include "mmu.h"
 #include "pgtable.h"
 
 static struct pkvm_pgtable hyp_mmu;
+static struct pkvm_pool hyp_mmu_pool;
+
+static void *hyp_mmu_zalloc_page(void)
+{
+	return pkvm_alloc_pages(&hyp_mmu_pool, 0);
+}
+
+static void hyp_mmu_get_page(void *vaddr)
+{
+	pkvm_get_page(&hyp_mmu_pool, vaddr);
+}
+
+static void hyp_mmu_put_page(void *vaddr)
+{
+	pkvm_put_page(&hyp_mmu_pool, vaddr);
+}
+
+static int hyp_mmu_page_count(void *vaddr)
+{
+	return pkvm_page_count(vaddr);
+}
+
+static const struct pkvm_pgtable_mm_ops hyp_mmu_mm_ops = {
+	.zalloc_page = hyp_mmu_zalloc_page,
+	.get_page = hyp_mmu_get_page,
+	.put_page = hyp_mmu_put_page,
+	.page_count = hyp_mmu_page_count,
+};
 
 static bool hyp_mmu_pte_present(void *ptep)
 {
@@ -104,6 +133,41 @@ static const struct pkvm_pgtable_ops hyp_mmu_pgt_ops = {
 	.flush_tlb = hyp_mmu_flush_tlb,
 };
 
+static int fix_hyp_mmu_refcnt_walker(struct pkvm_pgtable_visit_ctx *ctx,
+				     unsigned long walk_flags,
+				     void *const arg)
+{
+	if (!ctx->pgt->pgt_ops->pte_present(ctx->ptep))
+		return 0;
+
+	/*
+	 * Fix-up the refcount for the page-table pages as the early allocator
+	 * was unable to access the pkvm_vmemmap and so the buddy allocator has
+	 * initialized the refcount to '1'.
+	 */
+	hyp_mmu_get_page(ctx->ptep);
+
+	return 0;
+}
+
+static int fix_hyp_mmu_page_refcnt(void)
+{
+	struct pkvm_pgtable_walker walker = {
+		.cb = fix_hyp_mmu_refcnt_walker,
+		.arg = NULL,
+		.walk_flags = PKVM_PGTABLE_WALK_LEAF | PKVM_PGTABLE_WALK_TABLE_POST,
+	};
+	unsigned long size;
+
+	/*
+	 * Calculate the max address space, then walk the [0, size) address
+	 * range to fixup refcount of every page-table page.
+	 */
+	size = hyp_mmu.pgt_ops->level_to_size(hyp_mmu.cap.level + 1);
+
+	return pkvm_pgtable_walk(&hyp_mmu, 0, size, &walker);
+}
+
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages)
 {
 	struct pkvm_pgtable_cap cap = {
@@ -118,6 +182,42 @@ int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages)
 	pkvm_early_alloc_init(pool_base, pool_pages << PAGE_SHIFT);
 
 	return pkvm_pgtable_init(&hyp_mmu, cap, &pkvm_early_alloc_mm_ops, &hyp_mmu_pgt_ops);
+}
+
+int pkvm_hyp_mmu_switch_to_buddy(void *pool_base, unsigned long pool_pages)
+{
+	unsigned long reserved_pages;
+	int ret;
+
+	/* Get the early alloc used pages and reserve them in hyp_mmu_pool */
+	if (hyp_mmu.mm_ops != &pkvm_early_alloc_mm_ops)
+		return -EINVAL;
+
+	reserved_pages = pkvm_early_alloc_nr_used_pages();
+	/* Enable buddy allocator */
+	ret = pkvm_pool_init(&hyp_mmu_pool, __pkvm_pa(pool_base) >> PAGE_SHIFT,
+			     pool_pages, reserved_pages);
+	if (ret)
+		return ret;
+
+	if (reserved_pages) {
+		/*
+		 * As the early alloc mm_ops was used to allocate mmu
+		 * page-table pages, the refcount of each page-table pages
+		 * was not maintained at that time. This should be fixed.
+		 */
+		ret = fix_hyp_mmu_page_refcnt();
+		if (ret)
+			return ret;
+	}
+
+	hyp_mmu.mm_ops = &hyp_mmu_mm_ops;
+	return 0;
+}
+
+void pkvm_hyp_mmu_load(void)
+{
+	native_write_cr3(hyp_mmu.root_pa);
 }
 
 int pkvm_hyp_mmu_finalize(hyp_mmu_finalize_fn_t fn)

arch/x86/kvm/pkvm/early_alloc.c
@@ -3,6 +3,7 @@
 #include <asm/kvm_pkvm.h>
 #include <asm/pkvm_spinlock.h>
 #include <asm/string.h>
+#include "early_alloc.h"
 #include "pgtable.h"
 
 static unsigned long base;
@@ -62,4 +63,9 @@ void pkvm_early_alloc_init(void *virt, unsigned long size)
 {
 	base = cur = (unsigned long)virt;
 	end = base + size;
+}
+
+unsigned long pkvm_early_alloc_nr_used_pages(void)
+{
+	return (cur - base) >> PAGE_SHIFT;
 }

arch/x86/kvm/pkvm/early_alloc.h
@@ -6,4 +6,6 @@
 
 extern const struct pkvm_pgtable_mm_ops pkvm_early_alloc_mm_ops;
 
+unsigned long pkvm_early_alloc_nr_used_pages(void);
+
 #endif /* __PKVM_EARLY_ALLOC_H */

arch/x86/kvm/pkvm/init_finalize.c
@@ -122,7 +122,15 @@ static int create_hyp_mmu(const struct pkvm_mem_info infos[], int nr_infos)
 		}
 	}
 
-	return back_vmemmap(__pkvm_pa(pkvm_vmemmap_base));
+	ret = back_vmemmap(__pkvm_pa(pkvm_vmemmap_base));
+	if (ret)
+		return ret;
+
+	/* Load pKVM hypervisor's MMU to use pKVM vmemmap */
+	pkvm_hyp_mmu_load();
+
+	/* Update the pKVM mmu to use pKVM buddy allocator */
+	return pkvm_hyp_mmu_switch_to_buddy(hyp_pgt_base, nr_pages);
 }
 
 static int finalize_global(struct pkvm_mem_info infos[], int nr_infos)
@@ -166,6 +174,13 @@ int pkvm_init_finalize(struct pkvm_mem_info infos[], int nr_infos,
 			return ret;
 
 		global_finalized = true;
+	} else {
+		/*
+		 * The pKVM hypervisor's MMU was already loaded on the first
+		 * finalized CPU during the global finalize. Need to load it
+		 * on all the other CPUs as well.
+		 */
+		pkvm_hyp_mmu_load();
 	}
 
 	return pkvm_hyp_mmu_finalize(hyp_mmu_finalize_fn);

arch/x86/kvm/pkvm/mmu.h
@@ -5,6 +5,8 @@
 #include "init_finalize.h"
 
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages);
+int pkvm_hyp_mmu_switch_to_buddy(void *pool_base, unsigned long pool_pages);
+void pkvm_hyp_mmu_load(void);
 int pkvm_hyp_mmu_finalize(hyp_mmu_finalize_fn_t fn);
 int pkvm_hyp_mmu_map(unsigned long vaddr, unsigned long phys,
 		     unsigned long size, u64 prot);

maluka-dmytro/pKVM-IA

commit 70f2742a395b1ec91c0ee1b8e47610d5d98869b7

SQUASHME: pKVM: x86: Introduce pkvm_pgtable_max_size()
We already have a couple of places where we calculate the max size of
the VA space that can be mapped by the given page table, and with guest
MMU there will be more such places. So introduce helper function
pkvm_pgtable_max_size() for that.

Fixes: ca66f34a8120 ("pKVM: x86: Switch to use buddy allocator for the hyp mmu")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/mmu.c
@@ -181,7 +181,7 @@ static int fix_hyp_mmu_page_refcnt(void)
 	 * Calculate the max address space, then walk the [0, size) address
 	 * range to fixup refcount of every page-table page.
 	 */
-	size = hyp_mmu.pgt_ops->level_to_size(hyp_mmu.cap.level + 1);
+	size = pkvm_pgtable_max_size(&hyp_mmu);
 #endif
 
 	return pkvm_pgtable_walk(&hyp_mmu, 0, size, &walker);

arch/x86/kvm/pkvm/pgtable.h
@@ -200,4 +200,13 @@ static inline enum pkvm_owner_id pkvm_pte_owner_id(struct pkvm_pgtable *pgt, voi
 	return FIELD_GET(PKVM_INVALID_PTE_OWNER_MASK, pgt->pgt_ops->pte_get(ptep));
 }
 
+/*
+ * Return the max size of the virtual address space that can be
+ * mapped by the page table @pgt.
+ */
+static inline unsigned long pkvm_pgtable_max_size(struct pkvm_pgtable *pgt)
+{
+	return pgt->pgt_ops->level_to_size(pgt->cap.level + 1);
+}
+
 #endif /* __PKVM_X86_PGTABLE_H */