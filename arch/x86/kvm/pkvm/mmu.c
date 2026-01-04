cxdong/pub-pKVM-IA

commit 7666056852cabb221412882d8e2d2484ea46cbfe

pKVM: x86: Set the initial page state for host MMIO pages
The host MMIO pages in the lower address range e.g., 2GB ~ 4GB are
mapped in the host mmu. Mark the PKVM_PAGE_OWNED for these mapped MMIO
leaf entries.

A non-present pte will be used to the pKVM hypervisor to install MMIO
mappings at the running time. Set the page state as PKVM_PAGE_NONE to as
it is non-present leaf. Meanwhile set the owner ID as PKVM_ID_HYP so
that only the pKVM hypervisor can initiate the transition of this MMIO
page to the host.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/mmu.c
@@ -256,6 +256,67 @@ static int check_page_owner(struct pkvm_pgtable *pgt, unsigned long vaddr,
 	return pkvm_pgtable_walk(pgt, vaddr, size, &walker);
 }
 
+static int fix_host_mmu_pgstate_walker(struct pkvm_pgtable_visit_ctx *ctx,
+				       unsigned long walk_flags,
+				       void *const arg)
+{
+	const struct pkvm_pgtable_ops *pgt_ops = ctx->pgt->pgt_ops;
+	u64 pte;
+
+	if (pgt_ops->pte_present(ctx->ptep)) {
+		/* A present pte could map either memory pages or MMIO pages. */
+		unsigned long phys, size;
+
+		phys = pgt_ops->pte_to_phys(ctx->ptep);
+		size = pgt_ops->level_to_size(ctx->level);
+
+		if (is_mmio_range(phys, size)) {
+			/*
+			 * Store PKVM_PAGE_OWNED page state in pte for the MMIO
+			 * pages. No need to flush TLB as the page state bits in
+			 * pte are ignored bits.
+			 */
+			pte = pgt_ops->pte_get(ctx->ptep) |
+			      pgt_ops->pte_mk_pgstate(PKVM_PAGE_OWNED);
+
+			pgt_ops->pte_set(ctx->ptep, pte);
+		} else {
+			/*
+			 * Not MMIO pages then should be memory pages. Otherwise
+			 * it is a code bug.
+			 */
+			BUG_ON(!is_memory_range(phys, size));
+			set_host_mem_pgstate(phys, size, PKVM_PAGE_OWNED);
+		}
+	} else {
+		/*
+		 * A non-present pte will be used by the pKVM hypervisor to
+		 * install MMIO mappings at runtime. Set the page state
+		 * as PKVM_PAGE_NONE to as it is non-present leaf. Meanwhile set
+		 * the owner ID as PKVM_ID_HYP so that only the pKVM hypervisor
+		 * can initiate the transition of this MMIO page to the host.
+		 */
+		pte = pgt_ops->pte_mk_pgstate(PKVM_PAGE_NONE) |
+		      pkvm_pte_mk_owner_id(PKVM_ID_HYP);
+
+		pgt_ops->pte_set(ctx->ptep, pte);
+	}
+
+	return 0;
+}
+
+static int fix_host_mmu_pgstate(void)
+{
+	unsigned long size = host_mmu.pgt_ops->level_to_size(host_mmu.cap.level + 1);
+	struct pkvm_pgtable_walker walker = {
+		.cb = fix_host_mmu_pgstate_walker,
+		.arg = NULL,
+		.walk_flags = PKVM_PGTABLE_WALK_LEAF,
+	};
+
+	return pkvm_pgtable_walk(&host_mmu, 0, size, &walker);
+}
+
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages)
 {
 	struct pkvm_pgtable_cap cap = {
@@ -355,9 +416,6 @@ int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages, host_mmu_init_
 		if (ret)
 			return ret;
 
-		set_host_mem_pgstate((unsigned long)reg->base,
-				     (unsigned long)reg->size,
-				     PKVM_PAGE_OWNED);
 	}
 
 	/* Map holes between memblocks as MMIO with RWX permissions */
@@ -369,7 +427,7 @@ int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages, host_mmu_init_
 			return ret;
 	}
 
-	return 0;
+	return fix_host_mmu_pgstate();
 }
 
 int pkvm_host_mmu_finalize(host_mmu_finalize_fn_t fn)

maluka-dmytro/pKVM-IA

commit 52454e17c9def0f02d5e1b3d1ae1b7f17ad025ea

SQUASHME: pKVM: x86: Use pkvm_pgtable_max_size() in fix_host_mmu_pgstate()
Fixes: 7666056852ca ("pKVM: x86: Set the initial page state for host MMIO pages")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/mmu.c
@@ -320,14 +320,14 @@ static int fix_host_mmu_pgstate_walker(struct pkvm_pgtable_visit_ctx *ctx,
 
 static int fix_host_mmu_pgstate(void)
 {
-	unsigned long size = host_mmu.pgt_ops->level_to_size(host_mmu.cap.level + 1);
 	struct pkvm_pgtable_walker walker = {
 		.cb = fix_host_mmu_pgstate_walker,
 		.arg = NULL,
 		.walk_flags = PKVM_PGTABLE_WALK_LEAF,
 	};
 
-	return pkvm_pgtable_walk(&host_mmu, 0, size, &walker);
+	return pkvm_pgtable_walk(&host_mmu, 0, pkvm_pgtable_max_size(&host_mmu),
+				 &walker);
 }
 
 static int host_mmu_map(unsigned long phys, unsigned long size, bool mmio)