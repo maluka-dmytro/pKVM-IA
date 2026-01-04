cxdong/pub-pKVM-IA

commit 54b305061b07e6d7164c1170b1e7e4258167430f

pKVM: x86: Set initial host memory page state
The host memory page state is stored in the pkvm_page structure. Set the
page state as PKVM_PAGE_OWNED for all the mapped memory pages during the
host mmu initialization.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/mmu.c
@@ -180,6 +180,18 @@ static int fix_hyp_mmu_page_refcnt(void)
 	return pkvm_pgtable_walk(&hyp_mmu, 0, size, &walker);
 }
 
+static void set_host_mem_pgstate(unsigned long phys, unsigned long size,
+				 enum pkvm_page_state pgstate)
+{
+	unsigned long end = PAGE_ALIGN(phys + size);
+	struct pkvm_page *page;
+
+	for (phys = PAGE_ALIGN_DOWN(phys); phys < end; phys += PAGE_SIZE) {
+		page = pkvm_phys_to_page(phys);
+		page->host_state = pgstate;
+	}
+}
+
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages)
 {
 	struct pkvm_pgtable_cap cap = {
@@ -278,6 +290,10 @@ int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages, host_mmu_init_
 					true, true, true, false);
 		if (ret)
 			return ret;
+
+		set_host_mem_pgstate((unsigned long)reg->base,
+				     (unsigned long)reg->size,
+				     PKVM_PAGE_OWNED);
 	}
 
 	/* Map holes between memblocks as MMIO with RWX permissions */

maluka-dmytro/pKVM-IA

commit ce1321eaaeb13a3eae138f5760901eeb414ba254

SQUASHME: pKVM: x86: Introduce for_each_pkvm_page() helper
We already have several places in the code where we traverse struct
pkvm_page structures for all pages in the given memory range. With guest
MMU support there will be even more such cases.

So introduce a convenient helper macro for_each_pkvm_page() for that, to
reduce code duplication and improve code readability.

Based on pKVM-ARM's for_each_hyp_page(), although the implementation is
not exactly the same.

Fixes: 54b305061b07 ("pKVM: x86: Set initial host memory page state")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/memory.h
@@ -44,6 +44,12 @@ extern u64 __pkvm_vmemmap;
 #define pkvm_page_to_virt(page)	__pkvm_va(pkvm_page_to_phys(page))
 #define pkvm_page_to_pool(page)	(((struct pkvm_page *)page)->pool)
 
+/* Caution: __st is evaluated twice. */
+#define for_each_pkvm_page(__p, __st, __sz)						\
+	for (struct pkvm_page *__p = pkvm_phys_to_page(PAGE_ALIGN_DOWN(__st)),		\
+			      *__e = pkvm_phys_to_page(PAGE_ALIGN((__st) + (__sz)));	\
+	     __p < __e; __p++)
+
 /*
  * Refcounting for 'struct pkvm_page'.
  * pkvm_pool::lock must be held if atomic access to the refcount is required.

arch/x86/kvm/pkvm/mmu.c
@@ -190,13 +190,8 @@ static int fix_hyp_mmu_page_refcnt(void)
 static void set_host_mem_pgstate(unsigned long phys, unsigned long size,
 				 enum pkvm_page_state pgstate)
 {
-	unsigned long end = PAGE_ALIGN(phys + size);
-	struct pkvm_page *page;
-
-	for (phys = PAGE_ALIGN_DOWN(phys); phys < end; phys += PAGE_SIZE) {
-		page = pkvm_phys_to_page(phys);
+	for_each_pkvm_page(page, phys, size)
 		page->host_state = pgstate;
-	}
 }
 
 static int check_host_mem_pgstate(unsigned long phys, unsigned long size,