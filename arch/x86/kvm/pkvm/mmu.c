cxdong/pub-pKVM-IA

commit e365103448747220ff355a82435b9ae7b005f734

pKVM: x86: Remove pkvm_host_mmu_map/unmap
With the page state management, the memory/mmio pages mapping/unmapping
for host mmu should be done via the page protection functions rather
than pkvm_host_mmu_map/unmap, so that the mapping/unmapping operations
can be guarded by the page state. Remove the pkvm_host_mmu_map/unmap
functions.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/mmu.c
@@ -339,6 +339,15 @@ static int fix_host_mmu_pgstate(void)
 	return pkvm_pgtable_walk(&host_mmu, 0, size, &walker);
 }
 
+static int host_mmu_map(unsigned long phys, unsigned long size, bool mmio)
+{
+	u64 prot = host_mmu.pgt_ops->calc_pte_perm(true, true, true) |
+		   host_mmu.pgt_ops->calc_pte_memtype(mmio);
+
+	/* The vaddr == phys for the host MMU */
+	return pkvm_pgtable_map(&host_mmu, phys, phys, size, prot);
+}
+
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages)
 {
 	struct pkvm_pgtable_cap cap = {
@@ -432,9 +441,8 @@ int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages, host_mmu_init_
 	/* Map memory blocks with RWX permissions */
 	for (i = 0; i < pkvm_memblock_nr; i++) {
 		reg = &pkvm_memory[i];
-		ret = pkvm_host_mmu_map((unsigned long)reg->base,
-					(unsigned long)reg->size,
-					true, true, true, false);
+		ret = host_mmu_map((unsigned long)reg->base,
+				   (unsigned long)reg->size, false);
 		if (ret)
 			return ret;
 
@@ -443,8 +451,7 @@ int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages, host_mmu_init_
 	/* Map holes between memblocks as MMIO with RWX permissions */
 	for (i = phys = 0; i < pkvm_memblock_nr; i++, phys = reg->base + reg->size) {
 		reg = &pkvm_memory[i];
-		ret = pkvm_host_mmu_map(phys, (unsigned long)reg->base - phys,
-					true, true, true, true);
+		ret = host_mmu_map(phys, (unsigned long)reg->base - phys, true);
 		if (ret)
 			return ret;
 	}
@@ -455,22 +462,6 @@ int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages, host_mmu_init_
 int pkvm_host_mmu_finalize(host_mmu_finalize_fn_t fn)
 {
 	return fn ? fn(&host_mmu) : 0;
-}
-
-int pkvm_host_mmu_map(unsigned long phys, unsigned long size,
-		      bool read, bool write, bool exec, bool mmio)
-{
-	u64 prot = host_mmu.pgt_ops->calc_pte_perm(read, write, exec) |
-		   host_mmu.pgt_ops->calc_pte_memtype(mmio);
-
-	/* The vaddr == phys for the host MMU */
-	return pkvm_pgtable_map(&host_mmu, phys, phys, size, prot);
-}
-
-int pkvm_host_mmu_unmap(unsigned long vaddr, unsigned long size)
-{
-	/* The vaddr == phys for the host MMU */
-	return pkvm_pgtable_unmap(&host_mmu, vaddr, vaddr, size);
 }
 
 /**

arch/x86/kvm/pkvm/mmu.h
@@ -20,9 +20,6 @@ void pkvm_hyp_mmu_clone_host(unsigned long start_vaddr);
 int pkvm_host_mmu_init(void *pool_base, unsigned long pool_pages,
 		       host_mmu_init_fn_t fn);
 int pkvm_host_mmu_finalize(host_mmu_finalize_fn_t fn);
-int pkvm_host_mmu_map(unsigned long phys, unsigned long size, bool read,
-		      bool write, bool exec, bool mmio);
-int pkvm_host_mmu_unmap(unsigned long vaddr, unsigned long size);
 
 static inline void pkvm_host_mmu_lock(void)
 {

maluka-dmytro/pKVM-IA

commit 951038fa766680500c84a01dcec0a9e819b09e1e

SQUASHME: pKVM: x86: Use host_mmu_pte_prot() in host_mmu_map()
Fixes: e36510344874 ("pKVM: x86: Remove pkvm_host_mmu_map/unmap")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/mmu.c
@@ -338,11 +338,9 @@ static int fix_host_mmu_pgstate(void)
 
 static int host_mmu_map(unsigned long phys, unsigned long size, bool mmio)
 {
-	u64 prot = host_mmu.pgt_ops->calc_pte_perm(true, true, true) |
-		   host_mmu.pgt_ops->calc_pte_memtype(mmio);
-
 	/* The vaddr == phys for the host MMU */
-	return pkvm_pgtable_map(&host_mmu, phys, phys, size, prot);
+	return pkvm_pgtable_map(&host_mmu, phys, phys, size,
+				host_mmu_pte_prot(mmio));
 }
 
 int pkvm_hyp_mmu_init(void *pool_base, unsigned long pool_pages)