13e337f95a8655733b11cdb52bc3d72b74e4446a
Chenyi Qiang <chenyi.qiang@intel.com>
2026-01-06 10:35:38 +0800

    pKVM: VMX: Synchronize host disabled_exits to pKVM in npVMs
    Host userspace could control the VM exits for some operations like aperf
    mperf MSR read through the KVM_CAP_X86_DISALBE_EXITS capability before
    vCPU created. Synchronize the host disable_exits configuration to pKVM
    in npVMs during vCPU creation.
    
    This fixes the KVM kselftest (aperfmperf_test).
    
    Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>

--- arch/x86/kvm/pkvm/pkvm.c
+++ arch/x86/kvm/pkvm/pkvm.c
@@ -384,6 +384,9 @@ static int __vcpu_create(struct kvm *kvm, struct kvm_vcpu *vcpu, struct fpstate 
 		kvm->arch.notify_vmexit_flags = pkvm_vm->shared_kvm->arch.notify_vmexit_flags;
 	}
 
+	if (!pkvm_is_protected_vcpu(vcpu))
+		kvm->arch.disabled_exits = pkvm_vm->shared_kvm->arch.disabled_exits;
+
 	pkvm_spin_unlock(&pkvm_vm->lock);
 
 	vcpu->kvm = kvm;
