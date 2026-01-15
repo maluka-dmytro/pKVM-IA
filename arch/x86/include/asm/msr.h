cxdong/pub-pKVM-IA

commit bfd5dd8e37aa4e7f1b2142386dcaea14e6e2ba65

pKVM: VMX: Improve the host MSR vmexit handler
With the VMX MSR bitmap support, the MSR which the corresponding bit in
the bitmap is 1 will cause the vmexit to the pKVM. So if an MSR in the
bitmap, it should be emulated otherwise it is a code bug.

If an MSR out of the range of 00000000 ~ 00001FFFF and C00000000 ~
C0001FFF, accessing will cause vmexit unconditionally which was handled
by injecting #GP to the host. However such MSR may be implemented by the
platform. So to make sure these MSR can be emulated properly, use the
safe version MSR accessing wrapper to access this MSR first. If it is
supported, pass through the accessing result to the host. Otherwise
inject the #GP exception.

The symbols enabled by CONFIG_PARAVIRT/_XXL/_SPINLOCKS can be removed by
undefine these configs in the undef.h. However the symbols enabled by
CONFIG_TRACEPOINTS cannot be removed similarly due to the same reason
with CONFIG_BUG/CONFIG_GENERIC_BUG. Directly modify the corresponding
kernel's header files to remove these symbols for the pKVM hypervisor.

[ Dmytro: Remove symbols enabled by CONFIG_TRACEPOINTS ]

Co-developed-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/msr.h
@@ -43,7 +43,7 @@ struct saved_msrs {
 #include <asm/atomic.h>
 #include <linux/tracepoint-defs.h>
 
-#ifdef CONFIG_TRACEPOINTS
+#if defined(CONFIG_TRACEPOINTS) && !defined(__PKVM_HYP__)
 DECLARE_TRACEPOINT(read_msr);
 DECLARE_TRACEPOINT(write_msr);
 DECLARE_TRACEPOINT(rdpmc);

include/linux/tracepoint-defs.h
@@ -87,7 +87,7 @@ struct bpf_raw_event_map {
 #define DECLARE_TRACEPOINT(tp) \
 	extern struct tracepoint __tracepoint_##tp
 
-#ifdef CONFIG_TRACEPOINTS
+#if defined(CONFIG_TRACEPOINTS) && !defined(__PKVM_HYP__)
 # define tracepoint_enabled(tp) \
 	static_branch_unlikely(&(__tracepoint_##tp).key)
 #else

arch/x86/kvm/pkvm/undef.h
@@ -12,6 +12,9 @@
 #undef CONFIG_PRINTK
 #undef CONFIG_DEBUG_VIRTUAL
 #undef CONFIG_CALL_THUNKS_DEBUG
+#undef CONFIG_PARAVIRT
+#undef CONFIG_PARAVIRT_XXL
+#undef CONFIG_PARAVIRT_SPINLOCKS
 
 #define NOTRACE
 
@@ -41,6 +44,7 @@
 /*
 #undef CONFIG_BUG
 #undef CONFIG_GENERIC_BUG
+#undef CONFIG_TRACEPOINTS
 */
 
 #endif /* __PKVM_X86_UNDEF_H */

arch/x86/kvm/pkvm/vmx/host_vmx.c
@@ -64,16 +64,55 @@ static void handle_cr(struct kvm_vcpu *vcpu)
 	}
 }
 
+static bool is_msr_in_bitmap_range(unsigned long msr)
+{
+	return msr <= 0x1FFF || (msr >= 0xC0000000 && msr <= 0xC0001FFF);
+}
+
 static int handle_read_msr(struct kvm_vcpu *vcpu)
 {
-	kvm_inject_gp(vcpu, 0);
-	return X86EMUL_UNHANDLEABLE;
+	unsigned long msr = vcpu->arch.regs[VCPU_REGS_RCX];
+	u32 low, high;
+
+	/*
+	 * The MSR reading bitmap doesn't intercept any MSR. If the vmexit is
+	 * caused by such MSR in the range of the bitmap, it should be a code
+	 * bug.
+	 */
+	BUG_ON(is_msr_in_bitmap_range(msr));
+
+	if (rdmsr_safe(msr, &low, &high)) {
+		kvm_inject_gp(vcpu, 0);
+		return X86EMUL_UNHANDLEABLE;
+	}
+
+	vcpu->arch.regs[VCPU_REGS_RAX] = low;
+	vcpu->arch.regs[VCPU_REGS_RDX] = high;
+
+	return X86EMUL_CONTINUE;
 }
 
 static int handle_write_msr(struct kvm_vcpu *vcpu)
 {
-	kvm_inject_gp(vcpu, 0);
-	return X86EMUL_UNHANDLEABLE;
+	unsigned long msr = vcpu->arch.regs[VCPU_REGS_RCX];
+	u32 low, high;
+
+	/*
+	 * The MSR writing bitmap doesn't intercept any MSR. If the vmexit is
+	 * caused by such MSR in the range of the bitmap, it should be a code
+	 * bug.
+	 */
+	BUG_ON(is_msr_in_bitmap_range(msr));
+
+	low = vcpu->arch.regs[VCPU_REGS_RAX];
+	high = vcpu->arch.regs[VCPU_REGS_RDX];
+
+	if (wrmsr_safe(msr, low, high)) {
+		kvm_inject_gp(vcpu, 0);
+		return X86EMUL_UNHANDLEABLE;
+	}
+
+	return X86EMUL_CONTINUE;
 }
 
 static void handle_xsetbv(struct kvm_vcpu *vcpu)

maluka-dmytro/pKVM-IA

commit 0325ab0c7b642b5a84aec125e8ba4b3cc4c66a73

SQUASHME: pKVM: x86: Remove changes to tracepoint-defs.h
Remove the tracepoint_enabled checking from asm/msr.h to avoid changing
the generic header tracepoint-defs.h.

Fixes: bfd5dd8e37aa ("pKVM: VMX: Improve the host MSR vmexit handler")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/include/asm/msr.h
@@ -43,7 +43,12 @@ struct saved_msrs {
 #include <asm/atomic.h>
 #include <linux/tracepoint-defs.h>
 
-#if defined(CONFIG_TRACEPOINTS) && !defined(__PKVM_HYP__)
+#if defined(__PKVM_HYP__) && defined(CONFIG_TRACEPOINTS)
+#undef tracepoint_enabled
+#define tracepoint_enabled(tracepoint) false
+#endif
+
+#ifdef CONFIG_TRACEPOINTS
 DECLARE_TRACEPOINT(read_msr);
 DECLARE_TRACEPOINT(write_msr);
 DECLARE_TRACEPOINT(rdpmc);

include/linux/tracepoint-defs.h
@@ -87,7 +87,7 @@ struct bpf_raw_event_map {
 #define DECLARE_TRACEPOINT(tp) \
 	extern struct tracepoint __tracepoint_##tp
 
-#if defined(CONFIG_TRACEPOINTS) && !defined(__PKVM_HYP__)
+#if defined(CONFIG_TRACEPOINTS)
 # define tracepoint_enabled(tp) \
 	static_branch_unlikely(&(__tracepoint_##tp).key)
 #else