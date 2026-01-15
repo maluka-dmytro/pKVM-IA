cxdong/pub-pKVM-IA

commit 1f977847e4b4c8a6d4843ed0effda11207cb1651

pKVM: x86: Import the kernel find_bit and hweight libraries
Add support for bit operations in the pKVM hypervisor by importing the
kernel's find_bit and hweight libraries. The find_random_bit() is
re-implemented as an inline function to cause a build bug as this
function is not used by the pKVM hypervisor and the original
implementation requires external symbols which are not able to be
imported. So just causing a build bug to detect if it is used by the
pKVM hypervisor or not at the compiling time.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

include/linux/find.h
@@ -44,7 +44,16 @@ unsigned long _find_next_bit_le(const unsigned long *addr, unsigned
 				long size, unsigned long offset);
 #endif
 
+#ifndef __PKVM_HYP__
 unsigned long find_random_bit(const unsigned long *addr, unsigned long size);
+#else
+static inline unsigned long find_random_bit(const unsigned long *addr, unsigned long size)
+{
+	BUILD_BUG_ON(1);
+
+	return 0;
+}
+#endif
 
 #ifndef find_next_bit
 /**

arch/x86/kvm/pkvm/Makefile
@@ -30,7 +30,8 @@ quiet_cmd_patch_retpoline = PATCH_RETPOLINE $@
 
 kernel-lib			:= ../../../../lib
 pkvm-hyp-y			+= $(kernel-lib)/sort.o $(kernel-lib)/bsearch.o \
-				   $(kernel-lib)/extable.o
+				   $(kernel-lib)/extable.o $(kernel-lib)/find_bit.o \
+				   $(kernel-lib)/hweight.o
 pkvm-hyp-$(CONFIG_LIST_HARDENED) += $(kernel-lib)/list_debug.o
 
 kvm				:= ..

lib/find_bit.c
@@ -293,6 +293,7 @@ EXPORT_SYMBOL(_find_next_bit_le);
 
 #endif /* __BIG_ENDIAN */
 
+#ifndef __PKVM_HYP__
 /**
  * find_random_bit - find a set bit at random position
  * @addr: The address to base the search on
@@ -315,3 +316,4 @@ unsigned long find_random_bit(const unsigned long *addr, unsigned long size)
 	}
 }
 EXPORT_SYMBOL(find_random_bit);
+#endif /* !__PKVM_HYP__ */

maluka-dmytro/pKVM-IA

commit 40896fd42cf6ae0578c09fc937e95bb26153460d

SQUASHME: pKVM: x86: Revert changes in include/linux/find.h
The changes in include/linux/find.h to redefine find_random_bit() as a
static inline function to trigger a compiler failure is not necessary as
no user in the pKVM hypervisor needs to find random bits. And if there
is such a user in the future, the compiler can still report a link error
to prevent from generating a kernel image. So revert these changes.

[ Chuanxiao: commit message ]

Fixes: 1f977847e4b4 ("pKVM: x86: Import the kernel find_bit and hweight libraries")
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

include/linux/find.h
@@ -44,16 +44,7 @@ unsigned long _find_next_bit_le(const unsigned long *addr, unsigned
 				long size, unsigned long offset);
 #endif
 
-#ifndef __PKVM_HYP__
 unsigned long find_random_bit(const unsigned long *addr, unsigned long size);
-#else
-static inline unsigned long find_random_bit(const unsigned long *addr, unsigned long size)
-{
-	BUILD_BUG_ON(1);
-
-	return 0;
-}
-#endif
 
 #ifndef find_next_bit
 /**