maluka-dmytro/pKVM-IA

commit be9e6983cf53ee9e76f3216419fccae40950feb7

pKVM: x86: Redefine KVM print macros for the pKVM hypervisor
The KVM print macros may be included in the code which are leveraged
by the pKVM hypervisor. Overridden these print macros to use the "pkvm"
prefixes, and avoid printing current task information which is not
supported in the pKVM hypervisor.

The symbols enabled by CONFIG_DYNAMIC_DEBUG/_CORE cannot be removed by
undefine these kernel configs in the undef.h due to the same reason with
the CONFIG_BUG/CONFIG_GENERIC_BUG. Directly modify the corresponding
kernel's header files to remove these symbols for the pKVM hypervisor.

[ Dmytro: Remove symbols enabled by CONFIG_DYNAMIC_DEBUG/_CORE ]

Co-developed-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Dmytro Maluka <dmaluka@google.com>
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

include/linux/kvm_host.h
@@ -878,6 +878,18 @@ struct kvm {
 	char stats_id[KVM_STATS_NAME_SIZE];
 };
 
+#ifdef __PKVM_HYP__
+#define kvm_err(fmt, ...) \
+	pr_err("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_info(fmt, ...) \
+	pr_info("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_debug(fmt, ...) \
+	pr_debug("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_debug_ratelimited(fmt, ...) \
+	pr_debug_ratelimited("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_pr_unimpl(fmt, ...) \
+	pr_err_ratelimited("pkvm: " fmt, ## __VA_ARGS__)
+#else
 #define kvm_err(fmt, ...) \
 	pr_err("kvm [%i]: " fmt, task_pid_nr(current), ## __VA_ARGS__)
 #define kvm_info(fmt, ...) \
@@ -890,6 +902,7 @@ struct kvm {
 #define kvm_pr_unimpl(fmt, ...) \
 	pr_err_ratelimited("kvm [%i]: " fmt, \
 			   task_tgid_nr(current), ## __VA_ARGS__)
+#endif
 
 /* The guest did something we don't support. */
 #define vcpu_unimpl(vcpu, fmt, ...)					\

arch/x86/kvm/pkvm/undef.h
@@ -54,6 +54,8 @@
 #undef CONFIG_GENERIC_BUG
 #undef CONFIG_TRACEPOINTS
 #undef CONFIG_DEBUG_PREEMPT
+#undef CONFIG_DYNAMIC_DEBUG
+#undef CONFIG_DYNAMIC_DEBUG_CORE
 */
 
 #endif /* __PKVM_X86_UNDEF_H */

include/linux/dynamic_debug.h
@@ -134,8 +134,9 @@ struct ddebug_class_param {
  * pr_debug() and friends are globally enabled or modules have selectively
  * enabled them.
  */
-#if defined(CONFIG_DYNAMIC_DEBUG) || \
-	(defined(CONFIG_DYNAMIC_DEBUG_CORE) && defined(DYNAMIC_DEBUG_MODULE))
+#if (defined(CONFIG_DYNAMIC_DEBUG) || \
+	(defined(CONFIG_DYNAMIC_DEBUG_CORE) && defined(DYNAMIC_DEBUG_MODULE))) && \
+	(!defined(__PKVM_HYP__) || defined(CONFIG_PKVM_X86_DEBUG))
 
 extern __printf(2, 3)
 void __dynamic_pr_debug(struct _ddebug *descriptor, const char *fmt, ...);

include/linux/printk.h
@@ -735,8 +735,9 @@ struct pi_entry {
 #endif
 
 /* If you are writing a driver, please use dev_dbg instead */
-#if defined(CONFIG_DYNAMIC_DEBUG) || \
-	(defined(CONFIG_DYNAMIC_DEBUG_CORE) && defined(DYNAMIC_DEBUG_MODULE))
+#if (defined(CONFIG_DYNAMIC_DEBUG) || \
+	(defined(CONFIG_DYNAMIC_DEBUG_CORE) && defined(DYNAMIC_DEBUG_MODULE))) && \
+	(!defined(__PKVM_HYP__) || defined(CONFIG_PKVM_X86_DEBUG))
 /* descriptor check is first to prevent flooding with "callbacks suppressed" */
 #define pr_debug_ratelimited(fmt, ...)					\
 do {									\

commit b1843f3f677944d71879b63289320fcf5aa0fbc5

SQUASHME: pKVM: x86: Remove changes to arch-independent kvm_host.h
Fixes: be9e6983cf53 ("pKVM: x86: Redefine KVM print macros for the pKVM hypervisor")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/include/asm/kvm_pkvm.h
@@ -488,7 +488,33 @@ static inline size_t pkvm_guest_initial_fpstate_size(struct kvm *kvm)
 
 #ifdef __PKVM_HYP__
 
-#ifndef CONFIG_PKVM_X86_DEBUG
+#undef kvm_err
+#undef kvm_info
+#undef kvm_debug
+#undef kvm_debug_ratelimited
+#undef kvm_pr_unimpl
+
+#ifdef CONFIG_PKVM_X86_DEBUG
+
+#define kvm_err(fmt, ...) \
+	pr_err("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_info(fmt, ...) \
+	pr_info("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_debug(fmt, ...) \
+	pr_debug("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_debug_ratelimited(fmt, ...) \
+	pr_debug_ratelimited("pkvm: " fmt, ## __VA_ARGS__)
+#define kvm_pr_unimpl(fmt, ...) \
+	pr_err_ratelimited("pkvm: " fmt, ## __VA_ARGS__)
+
+#else /* CONFIG_PKVM_X86_DEBUG */
+
+#define kvm_err(fmt, ...) do {} while(0)
+#define kvm_info(fmt, ...) do {} while(0)
+#define kvm_debug(fmt, ...) do {} while(0)
+#define kvm_debug_ratelimited(fmt, ...) do {} while(0)
+#define kvm_pr_unimpl(fmt, ...) do {} while(0)
+
 #undef WARN_ON
 #undef WARN
 #undef WARN_ON_ONCE
@@ -510,6 +536,7 @@ static inline size_t pkvm_guest_initial_fpstate_size(struct kvm *kvm)
 #define WARN_ONCE(condition, format...) WARN(condition, format)
 
 #define _BUG_FLAGS(ins, flags, extra)  asm volatile(ins)
+
 #endif /* CONFIG_PKVM_X86_DEBUG */
 
 #endif /* __PKVM_HYP__ */

include/linux/kvm_host.h
@@ -878,26 +878,6 @@ struct kvm {
 	char stats_id[KVM_STATS_NAME_SIZE];
 };
 
-#ifdef __PKVM_HYP__
-#ifdef CONFIG_PKVM_X86_DEBUG
-#define kvm_err(fmt, ...) \
-	pr_err("pkvm: " fmt, ## __VA_ARGS__)
-#define kvm_info(fmt, ...) \
-	pr_info("pkvm: " fmt, ## __VA_ARGS__)
-#define kvm_debug(fmt, ...) \
-	pr_debug("pkvm: " fmt, ## __VA_ARGS__)
-#define kvm_debug_ratelimited(fmt, ...) \
-	pr_debug_ratelimited("pkvm: " fmt, ## __VA_ARGS__)
-#define kvm_pr_unimpl(fmt, ...) \
-	pr_err_ratelimited("pkvm: " fmt, ## __VA_ARGS__)
-#else
-#define kvm_err(fmt, ...) do {} while(0)
-#define kvm_info(fmt, ...) do {} while(0)
-#define kvm_debug(fmt, ...) do {} while(0)
-#define kvm_debug_ratelimited(fmt, ...) do {} while(0)
-#define kvm_pr_unimpl(fmt, ...) do {} while(0)
-#endif
-#else
 #define kvm_err(fmt, ...) \
 	pr_err("kvm [%i]: " fmt, task_pid_nr(current), ## __VA_ARGS__)
 #define kvm_info(fmt, ...) \
@@ -910,7 +890,6 @@ struct kvm {
 #define kvm_pr_unimpl(fmt, ...) \
 	pr_err_ratelimited("kvm [%i]: " fmt, \
 			   task_tgid_nr(current), ## __VA_ARGS__)
-#endif
 
 /* The guest did something we don't support. */
 #define vcpu_unimpl(vcpu, fmt, ...)					\