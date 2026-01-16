cxdong/pub-pKVM-IA

commit 1f977847e4b4c8a6d4843ed0effda11207cb1651

pKVM: x86: Import the kernel find_bit and hweight libraries
Add support for bit operations in the pKVM hypervisor by importing the
kernel's find_bit and hweight libraries. The find_random_bit() is
re-implemented as an inline function to cause a build bug as this
function is not used by the pKVM hypervisor and the original
implementation requires external symbols which are not able to be
imported. So just causing a build bug to detect if it is used by the
pKVM hypervisor or not at the compiling time.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/Makefile
@@ -30,7 +30,8 @@ quiet_cmd_patch_retpoline = PATCH_RETPOLINE $@
 
 kernel-lib			:= ../../../../lib
 pkvm-hyp-y			+= $(kernel-lib)/sort.o $(kernel-lib)/bsearch.o \
-				   $(kernel-lib)/extable.o
+				   $(kernel-lib)/extable.o $(kernel-lib)/find_bit.o \
+				   $(kernel-lib)/hweight.o
 pkvm-hyp-$(CONFIG_LIST_HARDENED) += $(kernel-lib)/list_debug.o
 
 kvm				:= ..

lib/find_bit.c
@@ -293,6 +293,7 @@ EXPORT_SYMBOL(_find_next_bit_le);
 
 #endif /* __BIG_ENDIAN */
 
+#ifndef __PKVM_HYP__
 /**
  * find_random_bit - find a set bit at random position
  * @addr: The address to base the search on
@@ -315,3 +316,4 @@ unsigned long find_random_bit(const unsigned long *addr, unsigned long size)
 	}
 }
 EXPORT_SYMBOL(find_random_bit);
+#endif /* !__PKVM_HYP__ */

include/linux/find.h
@@ -44,7 +44,16 @@ unsigned long _find_next_bit_le(const unsigned long *addr, unsigned
 				long size, unsigned long offset);
 #endif
 
+#ifndef __PKVM_HYP__
 unsigned long find_random_bit(const unsigned long *addr, unsigned long size);
+#else
+static inline unsigned long find_random_bit(const unsigned long *addr, unsigned long size)
+{
+	BUILD_BUG_ON(1);
+
+	return 0;
+}
+#endif
 
 #ifndef find_next_bit
 /**

maluka-dmytro/pKVM-IA

commit 82f3c043de66cae2c25b325b986d2c636ce99ea2

REORDER: pKVM: x86: find_bit: suit lib for pKVM needs
Use pKVM's version for find_bit lib after trimming it for pKVM needs:
- Remove __BIG_ENDIAN as they are not relevant for x86
- Remove problematic and not needed find_random_bit, which requires
  external symbols not available in pKVM.

Duplicating and freezing find_bit lib shouldn't be problematic longer
term as the API doesn't seem to change so often and within ten years
there are 25 patches rather extending it about new functionality rather
than changing API.

Fixes: 1f977847e4b4 ("pKVM: x86: Import the kernel find_bit and hweight libraries")
Change-Id: I528632b178dbdb20498159e20b4184b25044fc1a
Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

arch/x86/kvm/pkvm/Makefile
@@ -29,7 +29,8 @@ quiet_cmd_patch_retpoline = PATCH_RETPOLINE $@
       cmd_patch_retpoline = $(BASH) $(src)/compiling_cmds.sh patch_retpoline $< $@
 
 kernel-lib			:= ../../../../lib
-pkvm-hyp-y			+= $(kernel-lib)/bsearch.o $(kernel-lib)/find_bit.o \
+pkvm-lib			:= lib/
+pkvm-hyp-y			+= $(kernel-lib)/bsearch.o $(pkvm-lib)/find_bit.o \
 				   $(kernel-lib)/hweight.o $(kernel-lib)/string.o \
 				   $(kernel-lib)/ctype.o $(kernel-lib)/sort.o
 pkvm-hyp-$(CONFIG_LIST_HARDENED) += $(kernel-lib)/list_debug.o

arch/x86/kvm/pkvm/lib/find_bit.c
@@ -257,61 +257,3 @@ unsigned long find_next_clump8(unsigned long *clump, const unsigned long *addr,
 	return offset;
 }
 EXPORT_SYMBOL(find_next_clump8);
-
-#ifdef __BIG_ENDIAN
-
-#ifndef find_first_zero_bit_le
-/*
- * Find the first cleared bit in an LE memory region.
- */
-unsigned long _find_first_zero_bit_le(const unsigned long *addr, unsigned long size)
-{
-	return FIND_FIRST_BIT(~addr[idx], swab, size);
-}
-EXPORT_SYMBOL(_find_first_zero_bit_le);
-
-#endif
-
-#ifndef find_next_zero_bit_le
-unsigned long _find_next_zero_bit_le(const unsigned long *addr,
-					unsigned long size, unsigned long offset)
-{
-	return FIND_NEXT_BIT(~addr[idx], swab, size, offset);
-}
-EXPORT_SYMBOL(_find_next_zero_bit_le);
-#endif
-
-#ifndef find_next_bit_le
-unsigned long _find_next_bit_le(const unsigned long *addr,
-				unsigned long size, unsigned long offset)
-{
-	return FIND_NEXT_BIT(addr[idx], swab, size, offset);
-}
-EXPORT_SYMBOL(_find_next_bit_le);
-
-#endif
-
-#endif /* __BIG_ENDIAN */
-
-/**
- * find_random_bit - find a set bit at random position
- * @addr: The address to base the search on
- * @size: The bitmap size in bits
- *
- * Returns: a position of a random set bit; >= @size otherwise
- */
-unsigned long find_random_bit(const unsigned long *addr, unsigned long size)
-{
-	int w = bitmap_weight(addr, size);
-
-	switch (w) {
-	case 0:
-		return size;
-	case 1:
-		/* Performance trick for single-bit bitmaps */
-		return find_first_bit(addr, size);
-	default:
-		return find_nth_bit(addr, size, get_random_u32_below(w));
-	}
-}
-EXPORT_SYMBOL(find_random_bit);

lib/find_bit.c
@@ -293,7 +293,6 @@ EXPORT_SYMBOL(_find_next_bit_le);
 
 #endif /* __BIG_ENDIAN */
 
-#ifndef __PKVM_HYP__
 /**
  * find_random_bit - find a set bit at random position
  * @addr: The address to base the search on
@@ -316,4 +315,3 @@ unsigned long find_random_bit(const unsigned long *addr, unsigned long size)
 	}
 }
 EXPORT_SYMBOL(find_random_bit);
-#endif /* !__PKVM_HYP__ */