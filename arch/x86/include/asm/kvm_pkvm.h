maluka-dmytro/pKVM-IA

commit e07538e100860afc59dc9a9cdf955ca27cc05886

pKVM: VMX: Implement cache_reg operation
Import vmx_cache_reg() from KVM for pKVM to implement cache_reg
operation.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

include/linux/kvm_host.h
@@ -928,7 +928,11 @@ struct kvm {
 static inline void kvm_vm_dead(struct kvm *kvm)
 {
 	kvm->vm_dead = true;
+#ifndef __PKVM_HYP__
 	kvm_make_all_cpus_request(kvm, KVM_REQ_VM_DEAD);
+#else
+	/* TODO: Handle VM dead in the pKVM. */
+#endif
 }
 
 static inline void kvm_vm_bugged(struct kvm *kvm)

arch/x86/kvm/vmx/main.c
@@ -923,7 +923,9 @@ struct kvm_x86_ops vt_x86_ops __initdata = {
 	.set_gdt = vt_op(set_gdt),
 	.set_dr7 = vt_op(set_dr7),
 	.sync_dirty_debug_regs = vt_op(sync_dirty_debug_regs),
+#endif
 	.cache_reg = vt_op(cache_reg),
+#ifndef __PKVM_HYP__
 	.get_rflags = vt_op(get_rflags),
 	.set_rflags = vt_op(set_rflags),
 	.get_if_flag = vt_op(get_if_flag),

arch/x86/kvm/vmx/vmx.c
@@ -2636,7 +2636,6 @@ int vmx_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 	return ret;
 }
 
-#ifndef __PKVM_HYP__
 void vmx_cache_reg(struct kvm_vcpu *vcpu, enum kvm_reg reg)
 {
 	unsigned long guest_owned_bits;
@@ -2679,7 +2678,6 @@ void vmx_cache_reg(struct kvm_vcpu *vcpu, enum kvm_reg reg)
 		break;
 	}
 }
-#endif /* !__PKVM_HYP__ */
 
 /*
  * There is no X86_FEATURE for SGX yet, but anyway we need to query CPUID
@@ -3519,6 +3517,7 @@ void vmx_ept_load_pdptrs(struct kvm_vcpu *vcpu)
 		vmcs_write64(GUEST_PDPTR3, mmu->pdptrs[3]);
 	}
 }
+#endif /* !__PKVM_HYP__ */
 
 void ept_save_pdptrs(struct kvm_vcpu *vcpu)
 {
@@ -3535,6 +3534,7 @@ void ept_save_pdptrs(struct kvm_vcpu *vcpu)
 	kvm_register_mark_available(vcpu, VCPU_EXREG_PDPTR);
 }
 
+#ifndef __PKVM_HYP__
 #define CR3_EXITING_BITS (CPU_BASED_CR3_LOAD_EXITING | \
 			  CPU_BASED_CR3_STORE_EXITING)
 

commit 8404fa56d41e13c63d173e6fbd23f5413f50786e

SQUASHME: pKVM: x86: Remove remaining change to arch-independent kvm_host.h
Fixes: e07538e10086 ("pKVM: VMX: Implement cache_reg operation")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/include/asm/kvm_pkvm.h
@@ -539,6 +539,15 @@ static inline size_t pkvm_guest_initial_fpstate_size(struct kvm *kvm)
 
 #endif /* CONFIG_PKVM_X86_DEBUG */
 
+#undef KVM_BUG_ON
+#define KVM_BUG_ON(cond, kvm)						\
+({									\
+	bool __ret = !!(cond);						\
+									\
+	BUG_ON(__ret);							\
+	unlikely(__ret);						\
+})
+
 #endif /* __PKVM_HYP__ */
 
 #else /* !CONFIG_PKVM_X86 */

include/linux/kvm_host.h
@@ -907,11 +907,7 @@ struct kvm {
 static inline void kvm_vm_dead(struct kvm *kvm)
 {
 	kvm->vm_dead = true;
-#ifndef __PKVM_HYP__
 	kvm_make_all_cpus_request(kvm, KVM_REQ_VM_DEAD);
-#else
-	/* TODO: Handle VM dead in the pKVM. */
-#endif
 }
 
 static inline void kvm_vm_bugged(struct kvm *kvm)