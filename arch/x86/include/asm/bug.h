cxdong/pub-pKVM-IA

commit 4dda99fb11cc7c19d83cd2d7c2d39494f50ba74a

pKVM: x86: Rename all the pKVM hypervisor symbols
The pKVM hypervisor will be isolated from the linux kernel thus it
should not dynamically link to any linux kernel's symbols, but compile
the relevant code as part of the pKVM hypervisor. Some of the functions
in memset_64.c, x86.c and vmx.c are used by the pKVM hypervisor, e.g.,
memset(), kvm_inject_gp(), vmx_inject_exception(), vmxxx_error() and so
on. As these functions will also be required to support guest VMs later
so don't bother creating pKVM's owner functions for the host vm but
directly including these file, with stripping the unnecessary symbols
off from these files via using the define __PKVM_HYP__.

However, this will bring the symbol name conflicts because the linux
kernel also have the same file compiled. To resolve this, rename all the
pKVM hypervisor symbols via adding '__pkvm' suffix to avoid the
conflicts, except for the compiler generated symbols __x86_return_thunk
and __x86_indirect_XXX to satisfy the objtool and boot time patching
(the subsequent patch will introduce pKVM's own retpoline to support the
pKVM text boot time patching). The reason of adding suffix rather than
prefix is to not conflict with other prefixes like __cfi.

Another benefit of adding a suffix is that this can help caching the
incorrect linux kernel symbols usage in the pKVM hypervisor at the
linking time rather than at the running time, which is easier to debug
and fix.

As the pKVM hypervisor code doesn't export any symbol for the kernel to
use, by pass the  __EXPORT_SYMBOL for the pKVM hypervisor code to
strip off the export related symbols.

To strip off the unused kernel symbols from the pKVM, introduced
undef.h. This is included by each pKVM source code to undefine certain
kernel configs (except needs to define NOTRACE) to strip off certain
symbols, e.g, #undef CONFIG_PRINTK to remove printk. The kernel
CONFIG_BUG and CONFIG_GENERIC_BUG also introduced some unused symbols.

arch/x86/include/asm/bug.h
@@ -34,7 +34,7 @@
 #define BUG_UDB			0xffd6
 #define BUG_LOCK		0xfff0
 
-#ifdef CONFIG_GENERIC_BUG
+#if defined(CONFIG_GENERIC_BUG) && !defined(__PKVM_HYP__)
 
 #ifdef CONFIG_X86_32
 # define __BUG_REL(val)	".long " val

arch/x86/include/asm/kvm_pkvm.h
@@ -4,6 +4,7 @@
 
 #ifdef CONFIG_PKVM_X86
 #include <linux/mm.h>
+#include <asm/pkvm_image.h>
 
 #define PKVM_MEMBLOCK_REGIONS		128
 #define PKVM_STACK_SIZE			SZ_16K
@@ -25,9 +26,9 @@ struct pkvm_hyp {
 #define PKVM_PCPU_PAGES		(PAGE_ALIGN(sizeof(struct pkvm_pcpu)) >> PAGE_SHIFT)
 
 u64 pkvm_total_reserve_pages(void);
-void *pkvm_early_alloc_page(void);
-void *pkvm_early_alloc_contig(unsigned int nr_pages);
-void pkvm_early_alloc_init(void *virt, unsigned long size);
+PKVM_DECLARE(void *, pkvm_early_alloc_page, (void));
+PKVM_DECLARE(void *, pkvm_early_alloc_contig, (unsigned int nr_pages));
+PKVM_DECLARE(void, pkvm_early_alloc_init, (void *virt, unsigned long size));
 
 static inline unsigned long pkvm_data_pages(unsigned long extra_global,
 					    unsigned long extra_percpu)

include/asm-generic/bug.h
@@ -27,7 +27,7 @@ struct pt_regs;
 void __warn(const char *file, int line, void *caller, unsigned taint,
 	    struct pt_regs *regs, struct warn_args *args);
 
-#ifdef CONFIG_BUG
+#if defined(CONFIG_BUG) && !defined(__PKVM_HYP__)
 
 #ifdef CONFIG_GENERIC_BUG
 struct bug_entry {

include/linux/bug.h
@@ -28,7 +28,7 @@ struct pt_regs;
 
 #endif	/* __CHECKER__ */
 
-#ifdef CONFIG_GENERIC_BUG
+#if defined(CONFIG_GENERIC_BUG) && !defined(__PKVM_HYP__)
 #include <asm-generic/bug.h>
 
 static inline int is_warning_bug(const struct bug_entry *bug)

arch/x86/include/asm/pkvm_image.h
@@ -2,6 +2,26 @@
 #ifndef _ASM_X86_PKVM_IMAGE_H
 #define _ASM_X86_PKVM_IMAGE_H
 
+#if defined(__PKVM_HYP__)
+/*
+ * For the pKVM hypervisor code to use the pKVM hypervisor symbols.
+ * There is no need to manually add the suffix as the Makefile will
+ * automatically do this for symbols in the pKVM hypervisor
+ * text.
+ */
+#define PKVM_DECLARE(type, f, params)	type f params
+#define pkvm_sym(sym)			sym
+#else
+/*
+ * For the host code to use the pKVM hypervisor symbols which has
+ * the __pkvm suffix added by the Makefile. It is necessary to manually
+ * add the suffix as the Makefile will not do this for symbols in the
+ * host text.
+ */
+#define PKVM_DECLARE(type, f, params)	type f##__pkvm params
+#define pkvm_sym(sym)			sym##__pkvm
+#endif
+
 #ifdef LINKER_SCRIPT
 
 #define PKVM_SECTION_NAME(NAME)	.pkvm##NAME

arch/x86/kernel/vmlinux.lds.S
@@ -123,35 +123,35 @@ PHDRS {
 
 #define PKVM_TEXT							\
 	. = ALIGN(PAGE_SIZE);						\
-	__pkvm_text_start = .;						\
+	pkvm_sym(text_start) = .;						\
 	*(PKVM_SECTION_NAME(.text))					\
 	*(PKVM_SECTION_NAME(.noinstr.text))				\
 	. = ALIGN(PAGE_SIZE);						\
-	__pkvm_text_end = .;
+	pkvm_sym(text_end) = .;
 
 #define PKVM_BSS							\
 	. = ALIGN(PAGE_SIZE);						\
-	__pkvm_bss_start = .;						\
+	pkvm_sym(bss_start) = .;						\
 	*(PKVM_SECTION_NAME(.bss))					\
 	. = ALIGN(PAGE_SIZE);						\
-	__pkvm_bss_end = .;
+	pkvm_sym(bss_end) = .;
 
 #define PKVM_DATA							\
 	. = ALIGN(PAGE_SIZE);						\
-	__pkvm_data_start = .;						\
+	pkvm_sym(data_start) = .;						\
 	*(PKVM_SECTION_NAME(.data))					\
 	. = ALIGN(PAGE_SIZE);						\
-	__pkvm_data_end = .;
+	pkvm_sym(data_end) = .;
 
 #define PKVM_RODATA							\
 	PKVM_SECTION_NAME(.rodata) : 					\
 		AT(ADDR(PKVM_SECTION_NAME(.rodata)) - LOAD_OFFSET) {	\
 	. = ALIGN(PAGE_SIZE);						\
-	__pkvm_rodata_start = .;					\
+	pkvm_sym(rodata_start) = .;					\
 	*(PKVM_SECTION_NAME(.rodata))					\
 	*(PKVM_SECTION_NAME(.data..ro_after_init))			\
 	. = ALIGN(PAGE_SIZE);						\
-	__pkvm_rodata_end = .;						\
+	pkvm_sym(rodata_end) = .;						\
 	}
 #else
 

arch/x86/kvm/pkvm/Makefile
@@ -1,15 +1,30 @@
 # SPDX-License-Identifier: GPL-2.0
 
 ccflags-y += -I $(srctree)/arch/x86/kvm
+# TODO: make the stack protector work with the pKVM hypervisor code
+ccflags-y += -fno-stack-protector
+ccflags-y += -D__PKVM_HYP__
+ccflags-y += -include $(srctree)/arch/x86/kvm/pkvm/undef.h
 
-pkvm-hyp-y			:= early_alloc.o
+asflags-y += -D__PKVM_HYP__
 
-pkvm-hyp-$(CONFIG_PKVM_INTEL)	+= vmx/host_vmentry.o vmx/host_vmx.o
+cmd_deps			:= $(src)/compiling_cmds.sh
+
+pkvm-hyp-y			:= early_alloc.o pkvm.o
+
+kernel-x86-lib			:= ../../lib
+pkvm-hyp-y			+= $(kernel-x86-lib)/memset_64.o
+
+kvm				:= ..
+pkvm-hyp-y			+= $(kvm)/x86.o
+
+pkvm-hyp-$(CONFIG_PKVM_INTEL)	+= vmx/host_vmentry.o vmx/host_vmx.o \
+				   $(kvm)/vmx/vmx.o
 
 pkvm-obj 			:= $(patsubst %.o,%.pkvm.o,$(pkvm-hyp-y))
 obj-$(CONFIG_PKVM_X86)		+= pkvm.o
 
-targets += $(pkvm-obj) pkvm.lds
+targets += $(pkvm-obj) pkvm.tmp.o pkvm.lds
 
 $(obj)/%.pkvm.o: $(src)/%.c FORCE
 	$(call if_changed_rule,cc_o_c)
@@ -19,6 +34,28 @@ $(obj)/%.pkvm.o: $(src)/%.S FORCE
 $(obj)/pkvm.lds: $(src)/pkvm.lds.S FORCE
 	$(call if_changed_dep,cpp_lds_S)
 
-LDFLAGS_pkvm.o := -r -T
-$(obj)/pkvm.o: $(obj)/pkvm.lds $(addprefix $(obj)/,$(pkvm-obj)) FORCE
+LDFLAGS_pkvm.tmp.o := -r -T
+$(obj)/pkvm.tmp.o: $(obj)/pkvm.lds $(addprefix $(obj)/,$(pkvm-obj)) FORCE
 	$(call if_changed,ld)
+
+$(obj)/pkvm.o: $(obj)/pkvm.tmp.o $(cmd_deps) FORCE
+	$(call if_changed,pkvmcopy)
+
+quiet_cmd_pkvmcopy = PKVMPCOPY $@
+      cmd_pkvmcopy = $(BASH) $(src)/compiling_cmds.sh rename_syms $< $@
+
+# The pKVM hypervisor should be self-contained. All the symbols it uses should
+# be implemented in its own text. Remove ftrace to prevent compiler to insert
+# the __fentry__ symbols which are not part of the pKVM hypervisor text to
+# simplify the implementation.
+#
+# NOTE: do not remove CC_FLAGS_CFI. Since CFI code chunks don't depend
+# on the host kernel's global data, we can keep CFI coverage enabled
+# for the hypervisor as well, and we _should_ keep it enabled, for the
+# following reasons (apart from its security advantages):
+# 1. To avoid problems with the boot time kernel patching which assumes
+#    that if CFI is enabled, it covers the entire kernel text (see
+#    apply_seal_endbr()).
+# 2. To keep the calling convention compatible in cases when hypervisor
+#    functions are called from host kernel code (before deprivilege).
+KBUILD_CFLAGS := $(filter-out $(CC_FLAGS_FTRACE), $(KBUILD_CFLAGS))

arch/x86/kvm/pkvm/compiling_cmds.sh
@@ -1,0 +1,53 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+
+cmd="$1"
+
+rename_syms() {
+	local original_obj=$1
+	local renamed_obj=$2
+
+	# Generate a file which contains the original and new names for all
+	# necessary symbols. This will be used by the objcopy to rename symbols.
+	$NM $original_obj |
+	# Remove duplicated symbol names
+	$AWK '!seen[$NF]++ {print $NF}' |
+	# Filter out the names which contain:
+	# __x86_return_thunk
+	# __x86_indirect_
+	# The instructions using these symbols are generated by the compiler.
+	# Leave them alone to satisfy the objtool and the boot time patching for
+	# retpoline and return thunk.
+	$AWK '!/__x86_return_thunk/ && !/^__x86_indirect_/' |
+	# Rename the filtered symbols following below rules:
+	# 1) if a symbol name contains ".", which means it is generated by the
+	# compiler, add __pkvm suffix before the first "." to keep the compiler's
+	# suffix in place, e.g.: *.cold -> *__pkvm.cold.
+	# 2) otherwise just add "__pkvm" suffix.
+	#
+	# The reason we add __pkvm as a suffix, not a prefix, is to not conflict
+	# with other prefixes like __cfi which objtool and other tools rely on.
+	$AWK '{
+		idx = index($1, ".");
+		if (idx > 0) {
+			print $1, substr($1, 1, idx-1)"__pkvm"substr($1, idx)
+		} else {
+			print $1, $1"__pkvm"
+		}
+	}' > $renamed_obj.syms;
+
+	# Rename symbols for the original object according to the generated file
+	$OBJCOPY --redefine-syms=$renamed_obj.syms $original_obj $renamed_obj;
+
+	rm -f $renamed_obj.syms
+}
+
+case "$cmd" in
+	rename_syms)
+		if [ $# -ne 3 ];then
+			echo "Usage: $0 rename_syms <input object> <output object>"
+			exit 1
+		fi
+		rename_syms $2 $3;;
+	*)	die "Unknown command: $cmd";;
+esac

maluka-dmytro/pKVM-IA

commit c7d319cd7c619de2f4f16c2c5d7eedb927caf270

SQUASHME: pKVM: x86: Remove changes to bug.h
Fixes: 4dda99fb11cc ("pKVM: x86: Rename all the pKVM hypervisor symbols")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/include/asm/bug.h
@@ -34,8 +34,7 @@
 #define BUG_UDB			0xffd6
 #define BUG_LOCK		0xfff0
 
-#if defined(CONFIG_GENERIC_BUG) && \
-	(!defined(__PKVM_HYP__) || defined(CONFIG_PKVM_X86_DEBUG))
+#ifdef CONFIG_GENERIC_BUG
 
 #ifdef CONFIG_X86_32
 # define __BUG_REL(val)	".long " val

arch/x86/include/asm/kvm_pkvm.h
@@ -3,6 +3,7 @@
 #define _ASM_X86_KVM_PKVM_H
 
 #ifdef CONFIG_PKVM_X86
+#include <linux/bug.h>
 #include <linux/kvm_host.h>
 #include <linux/memblock.h>
 #include <linux/mm.h>
@@ -484,6 +485,34 @@ static inline size_t pkvm_guest_initial_fpstate_size(struct kvm *kvm)
 
 	return PAGE_ALIGN(size);
 }
+
+#ifdef __PKVM_HYP__
+
+#ifndef CONFIG_PKVM_X86_DEBUG
+#undef WARN_ON
+#undef WARN
+#undef WARN_ON_ONCE
+#undef WARN_ONCE
+#undef _BUG_FLAGS
+
+#define WARN_ON(condition) ({						\
+	int __ret_warn_on = !!(condition);				\
+	unlikely(__ret_warn_on);					\
+})
+
+#define WARN(condition, format...) ({					\
+	int __ret_warn_on = !!(condition);				\
+	no_printk(format);						\
+	unlikely(__ret_warn_on);					\
+})
+
+#define WARN_ON_ONCE(condition) WARN_ON(condition)
+#define WARN_ONCE(condition, format...) WARN(condition, format)
+
+#define _BUG_FLAGS(ins, flags, extra)  asm volatile(ins)
+#endif /* CONFIG_PKVM_X86_DEBUG */
+
+#endif /* __PKVM_HYP__ */
 
 #else /* !CONFIG_PKVM_X86 */
 

include/asm-generic/bug.h
@@ -27,8 +27,7 @@ struct pt_regs;
 void __warn(const char *file, int line, void *caller, unsigned taint,
 	    struct pt_regs *regs, struct warn_args *args);
 
-#if defined(CONFIG_BUG) && \
-	(!defined(__PKVM_HYP__) || defined(CONFIG_PKVM_X86_DEBUG))
+#ifdef CONFIG_BUG
 
 #ifdef CONFIG_GENERIC_BUG
 struct bug_entry {

include/linux/bug.h
@@ -28,8 +28,7 @@ struct pt_regs;
 
 #endif	/* __CHECKER__ */
 
-#if defined(CONFIG_GENERIC_BUG) && \
-	(!defined(__PKVM_HYP__) || defined(CONFIG_PKVM_X86_DEBUG))
+#ifdef CONFIG_GENERIC_BUG
 #include <asm-generic/bug.h>
 
 static inline int is_warning_bug(const struct bug_entry *bug)