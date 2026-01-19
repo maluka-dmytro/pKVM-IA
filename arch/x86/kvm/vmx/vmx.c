e59d58812d80e04ddf5d6cc121f4d130f0ac33b2
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-19 09:21:57 +0800

    pKVM: VMX: Update pVM vCPU state for EXIT_REASON_IO_INSTRUCTION
    The IO instruction vmexit for pVM is handled by the host. The pKVM
    hypervisor should update the pVM's RAX register with the emulated result
    from the host for IO in, and skip this IO instruction. This is not
    needed for npVM as npVM's RAX/RIP are already updated unconditionally.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -9583,12 +9583,31 @@ void pkvm_vmx_prepare_switch_to_host(struct kvm_vcpu *vcpu)
 	vmx_prepare_switch_to_host(to_vmx(vcpu));
 }
 
+static void update_protected_vcpu_state(struct kvm_vcpu *vcpu,
+					struct kvm_vcpu *shared_vcpu)
+{
+	switch (vmx_get_exit_reason(vcpu).basic) {
+	case EXIT_REASON_IO_INSTRUCTION:
+		/* Only need to update RAX for the input data */
+		if ((vmx_get_exit_qual(vcpu) & 8) != 0)
+			kvm_rax_write(vcpu, shared_vcpu->arch.regs[VCPU_REGS_RAX]);
+
+		WARN_ON_ONCE(kvm_skip_emulated_instruction(vcpu) != 1);
+		break;
+	default:
+		break;
+	}
+}
+
 static void pkvm_vmx_update_vcpu_state_from_host(struct kvm_vcpu *vcpu)
 {
-	/*
-	 * TODO: Update vcpu state according to the vmexit reason handled by
-	 * the host.
-	 */
+	struct vcpu_vmx *vmx = to_vmx(vcpu);
+
+	if (pkvm_is_protected_vcpu(vcpu) &&
+	    pkvm_has_req_to_host(HOST_HANDLE_EXIT, vcpu) &&
+	    !vmx_get_exit_reason(vcpu).failed_vmentry &&
+	    !vmx->fail)
+		update_protected_vcpu_state(vcpu, to_pkvm_vcpu(vcpu)->shared_vcpu);
 }
 
 static void share_nonprotected_vcpu_state(struct kvm_vcpu *vcpu,
