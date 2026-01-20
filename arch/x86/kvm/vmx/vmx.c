3521ce8fd328c2a5426c2bb60cd85e1a11428ac4
Chuanxiao Dong <chuanxiao.dong@intel.com>
2026-01-20 22:56:06 +0800

    SQUASHME: pKVM: x86: Fix compiling issue with CONFIG_PKVM_INTEL=n
    Fixes: e40778e1df0b ("KVM: pVMX: Implement sync_pir_to_irr operation")
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -7620,10 +7620,14 @@ int vmx_sync_pir_to_irr(struct kvm_vcpu *vcpu)
 	 * a VM-Exit and the subsequent entry will call sync_pir_to_irr.
 	 */
 	if (!is_guest_mode(vcpu) && kvm_vcpu_apicv_active(vcpu)) {
+#ifdef CONFIG_PKVM_INTEL
 		if (!enable_pkvm)
 			vmx_set_rvi(max_irr);
 		else if (max_irr != -1)
 			KVM_BUG_ON(pkvm_hypercall(sync_pir_to_irr, max_irr), vcpu->kvm);
+#else
+		vmx_set_rvi(max_irr);
+#endif
 	} else if (got_posted_interrupt) {
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
 	}
