maluka-dmytro/pKVM-IA

commit d57a07735ec8df344f3dd658942620e6d499693a

pKVM: x86: trace: Extend PV interface to dump the guest trace
Extend pkvm_dump_vmexit_trace() to dump the guest trace if the vm_handle
is set to be a valid guest VM handle. If it is a host VM handle, dump
the trace for the host VM and all the guest VMs.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/pkvm/trace.c
@@ -38,19 +38,67 @@ static void copy_vmexit_perf_data(struct perf_data *dst, struct vmexit_perf *per
 	pkvm_spin_unlock(&perf->lock);
 }
 
-static void copy_host_vm_trace(void *dst, unsigned long size)
+static void copy_host_vm_trace(void **dst, unsigned long *size)
 {
 	struct vmexit_perf *perf;
 	int cpu;
 
 	for_each_possible_cpu(cpu) {
 		perf = per_cpu_ptr(&hvcpu_perf, cpu);
-		if (size >= sizeof(struct perf_data)) {
-			copy_vmexit_perf_data(dst, perf);
-			dst += sizeof(struct perf_data);
-			size -= sizeof(struct perf_data);
+		if (*size >= sizeof(struct perf_data)) {
+			copy_vmexit_perf_data(*dst, perf);
+			*dst += sizeof(struct perf_data);
+			*size -= sizeof(struct perf_data);
 		}
 	}
+}
+
+struct copy_arg {
+	int vm_handle;
+	void *dst;
+	unsigned long size;
+};
+
+static int __copy_guest_vm_trace(struct pkvm_vm *vm, void *param)
+{
+	struct copy_arg *arg = param;
+	int i;
+
+	if (arg->vm_handle != PKVM_HOST_VM_HANDLE &&
+	    arg->vm_handle != vm->kvm.arch.pkvm.handle)
+		return 0;
+
+	pkvm_spin_lock(&vm->lock);
+	for (i = 0; i < vm->kvm.created_vcpus; i++) {
+		if (!vm->vcpus[i])
+			continue;
+
+		if (arg->size < sizeof(struct perf_data))
+			return -ENOSPC;
+
+		copy_vmexit_perf_data(arg->dst, &vm->vcpus[i]->perf);
+		arg->dst += sizeof(struct perf_data);
+		arg->size -= sizeof(struct perf_data);
+	}
+	pkvm_spin_unlock(&vm->lock);
+
+	/*
+	 * If vm_handle != PKVM_HOST_VM_HANDLE, it means the host wants to get
+	 * the trace for a specific guest VM, and the pKVM can stop dumping the
+	 * other guest VM's trace. Otherwise, the pKVM will continue.
+	 */
+	return arg->vm_handle != PKVM_HOST_VM_HANDLE;
+}
+
+static void copy_guest_vm_trace(int vm_handle, void *dst, unsigned long size)
+{
+	struct copy_arg arg = {
+		.vm_handle = vm_handle,
+		.dst = dst,
+		.size = size,
+	};
+
+	pkvm_walk_each_vm(__copy_guest_vm_trace, &arg);
 }
 
 void pkvm_trace_vmexit_start(struct kvm_vcpu *vcpu)
@@ -125,14 +173,19 @@ void pkvm_enable_vmexit_trace(bool en)
 	}
 }
 
-int pkvm_dump_vmexit_trace(phys_addr_t phys, unsigned long size)
+int pkvm_dump_vmexit_trace(phys_addr_t phys, unsigned long size, int vm_handle)
 {
 	int ret = pkvm_host_share_hyp(phys, size);
+	unsigned long dst_size = size;
+	void *dst = __pkvm_va(phys);
 
 	if (ret)
 		return ret;
 
-	copy_host_vm_trace(__pkvm_va(phys), size);
+	if (vm_handle == PKVM_HOST_VM_HANDLE)
+		copy_host_vm_trace(&dst, &dst_size);
+
+	copy_guest_vm_trace(vm_handle, dst, dst_size);
 
 	pkvm_host_unshare_hyp(phys, size);
 

arch/x86/kvm/pkvm/pkvm.c
@@ -1573,7 +1573,7 @@ void pkvm_handle_host_hypercall(struct kvm_vcpu *vcpu)
 		break;
 	case __pkvm__dump_vmexit_trace:
 		ret = pkvm_dump_vmexit_trace(pkvm_host_gpa_to_phys(pkvm_hc_input1(vcpu)),
-					     pkvm_hc_input2(vcpu));
+					     pkvm_hc_input2(vcpu), PKVM_HOST_VM_HANDLE);
 		break;
 	case __pkvm__check_processor_compatibility:
 		ret = kvm_x86_call(check_processor_compatibility)();
@@ -1749,4 +1749,25 @@ unsigned long pkvm_pcpu_tss(int cpu)
 void pkvm_x86_ops_init(struct pkvm_x86_ops *ops)
 {
 	memcpy(&pkvm_x86_ops, ops, sizeof(struct pkvm_x86_ops));
+}
+
+int pkvm_walk_each_vm(pkvm_vm_func_t func, void *arg)
+{
+	struct pkvm_vm *vm;
+	int i, ret = 0;
+
+	pkvm_spin_lock(&pkvm_vms_lock);
+
+	for (i = 0; i < MAX_PKVM_VMS; i++) {
+		vm = pkvm_vms_ref[i].pkvm_vm;
+		if (!vm)
+			continue;
+		ret = func(vm, arg);
+		if (ret)
+			break;
+	}
+
+	pkvm_spin_unlock(&pkvm_vms_lock);
+
+	return ret;
 }

arch/x86/kvm/pkvm/pkvm.h
@@ -131,5 +131,7 @@ int pkvm_vcpu_enter_guest(struct kvm_vcpu *vcpu, bool force_immediate_exit,
 			  unsigned long *reqs_to_host);
 void pkvm_x86_ops_init(struct pkvm_x86_ops *ops);
 int pkvm_emulate_hypercall(struct kvm_vcpu *vcpu);
+typedef int (*pkvm_vm_func_t)(struct pkvm_vm *vm, void *arg);
+int pkvm_walk_each_vm(pkvm_vm_func_t func, void *arg);
 
 #endif /* __PKVM_X86_PKVM_H */

arch/x86/kvm/pkvm/trace.h
@@ -8,6 +8,6 @@ void pkvm_trace_vmexit_start(struct kvm_vcpu *vcpu);
 void pkvm_trace_vmexit_end(struct kvm_vcpu *vcpu, u32 index);
 void pkvm_vcpu_perf_init(struct kvm_vcpu *vcpu);
 void pkvm_enable_vmexit_trace(bool en);
-int pkvm_dump_vmexit_trace(phys_addr_t pa, unsigned long size);
+int pkvm_dump_vmexit_trace(phys_addr_t phys, unsigned long size, int vm_handle);
 
 #endif

commit b24f5849e5e3f1d54718bf4e7fe6bef5266497a1

pKVM: x86: trace: Ensure resetting vmexit counters on time
Currently, when resetting pKVM vmexit trace by disabling it and enabling
it again ("echo 0 >enable_vmexit_trace; echo 1 >enable_vmexit_trace"),
resetting the vmexit perf counters for the given vCPU via
refresh_vmexit_perf() is not done immediately, it is only done in
pkvm_trace_vmexit_start() or pkvm_trace_vmexit_end() i.e. next time when
a vmexit happens on this vCPU. As a result, if there is a VM running
with a vCPU which is e.g. continuously sleeping on a HLT instruction and
thus is not generating any new vmexits, then after disabling & enabling
vmexit trace, this vCPU's perf counters are never reset i.e. remain with
their old (non-zero) values. So the stats shown by vmexit trace dump may
be misleading and inconsistent (showing new data for some vCPUs while
old data for other vCPUs).

One of the problems here is that enabling/disabling vmexit trace is
implemented per (physical) CPU. This doesn't fit well with guest vmexit
tracing, since a guest vCPU is not necessarily pinned to some pCPU.

So simplify the implementation by making this enable/disable control
global, not per CPU. And on top of that, fix the issue described above,
by refreshing vmexit perf counters immediately every time when enabling
vmexit trace, for all host vCPUs and for all guest vCPUs of all VMs.

Note that as a side effect, this change also slightly changes the
behavior: "echo 1 >enable_vmexit_trace" now always forces resetting the
perf counters, even if vmexit trace is actually already enabled, i.e.
without the need for prior "echo 0 >enable_vmexit_trace". This allows to
keep the code simpler, plus it may be actually convenient for the user.

Fixes: d57a07735ec8 ("pKVM: x86: trace: Extend PV interface to dump the guest trace")
Signed-off-by: Dmytro Maluka <dmaluka@google.com>

arch/x86/kvm/pkvm/trace.c
@@ -5,12 +5,8 @@
 #include "pkvm.h"
 #include "trace.h"
 
-struct perf_ctrl {
-	unsigned int age;
-	bool on;
-};
+static bool trace_on;
 static DEFINE_PER_CPU(struct vmexit_perf, hvcpu_perf);
-static DEFINE_PER_CPU(struct perf_ctrl, perf_ctrl);
 
 static inline bool is_host_vcpu(struct kvm_vcpu *vcpu)
 {
@@ -23,14 +19,41 @@ static inline struct vmexit_perf *vcpu_to_perf(struct kvm_vcpu *vcpu)
 				    &to_pkvm_vcpu(vcpu)->perf;
 }
 
-static void refresh_vmexit_perf(struct perf_ctrl *pctrl, struct vmexit_perf *perf)
+static void refresh_vmexit_perf(struct vmexit_perf *perf)
 {
 	pkvm_spin_lock(&perf->lock);
 	memset(perf->data.vmexit_reasons, 0, sizeof(perf->data.vmexit_reasons));
 	memset(perf->data.hypercalls, 0, sizeof(perf->data.hypercalls));
 	pkvm_spin_unlock(&perf->lock);
+}
 
-	perf->age = pctrl->age;
+static void refresh_host_vm_trace(void)
+{
+	int cpu;
+
+	for_each_possible_cpu(cpu)
+		refresh_vmexit_perf(per_cpu_ptr(&hvcpu_perf, cpu));
+}
+
+static int __refresh_guest_vm_trace(struct pkvm_vm *vm, void *param)
+{
+	int i;
+
+	pkvm_spin_lock(&vm->lock);
+	for (i = 0; i < vm->kvm.created_vcpus; i++) {
+		if (!vm->vcpus[i])
+			continue;
+
+		refresh_vmexit_perf(&vm->vcpus[i]->perf);
+	}
+	pkvm_spin_unlock(&vm->lock);
+
+	return 0;
+}
+
+static void refresh_guest_vm_trace(void)
+{
+	pkvm_walk_each_vm(__refresh_guest_vm_trace, NULL);
 }
 
 static void copy_vmexit_perf_data(struct perf_data *dst, struct vmexit_perf *perf)
@@ -105,15 +128,12 @@ static void copy_guest_vm_trace(int vm_handle, void *dst, unsigned long size)
 
 void pkvm_trace_vmexit_start(struct kvm_vcpu *vcpu)
 {
-	struct perf_ctrl *pctrl = this_cpu_ptr(&perf_ctrl);
 	struct vmexit_perf *perf;
 
-	if (!pctrl->on)
+	if (!READ_ONCE(trace_on))
 		return;
 
 	perf = vcpu_to_perf(vcpu);
-	if (pctrl->age != perf->age)
-		refresh_vmexit_perf(pctrl, perf);
 
 	perf->rax = vcpu->arch.regs[VCPU_REGS_RAX];
 	perf->tsc = rdtsc_ordered();
@@ -121,21 +141,16 @@ void pkvm_trace_vmexit_start(struct kvm_vcpu *vcpu)
 
 void pkvm_trace_vmexit_end(struct kvm_vcpu *vcpu, u32 reason)
 {
-	struct perf_ctrl *pctrl = this_cpu_ptr(&perf_ctrl);
 	struct vmexit_perf *perf;
 	unsigned long long cycles;
 
-	if (!pctrl->on)
+	if (!READ_ONCE(trace_on))
 		return;
 
 	if (reason >= MAX_EXIT_REASONS)
 		return;
 
 	perf = vcpu_to_perf(vcpu);
-	if (pctrl->age != perf->age) {
-		refresh_vmexit_perf(pctrl, perf);
-		return;
-	}
 
 	cycles = rdtsc_ordered() - perf->tsc;
 
@@ -165,14 +180,12 @@ void pkvm_vcpu_perf_init(struct kvm_vcpu *vcpu)
 
 void pkvm_enable_vmexit_trace(bool en)
 {
-	struct perf_ctrl *pctrl = this_cpu_ptr(&perf_ctrl);
-
-	if (en && !pctrl->on) {
-		pctrl->age++;
-		pctrl->on = true;
-	} else if (!en && pctrl->on) {
-		pctrl->on = false;
+	if (en) {
+		refresh_host_vm_trace();
+		refresh_guest_vm_trace();
 	}
+
+	WRITE_ONCE(trace_on, en);
 }
 
 int pkvm_dump_vmexit_trace(phys_addr_t phys, unsigned long size, int vm_handle)

arch/x86/kvm/pkvm_debugfs.c
@@ -6,24 +6,9 @@
 #include <asm/kvm_pkvm.h>
 #include <asm/pkvm_trace.h>
 
-static void enable_vmexit_trace_func(void *data)
-{
-	u64 val;
-
-	if (!data)
-		return;
-
-	val = *(u64 *)data;
-	pkvm_hypercall(enable_vmexit_trace, val);
-}
-
 static int enable_vmexit_trace(void *data, u64 val)
 {
-	int cpu;
-
-	for_each_possible_cpu(cpu)
-		smp_call_function_single(cpu, enable_vmexit_trace_func, &val, true);
-
+	pkvm_hypercall(enable_vmexit_trace, val);
 	return 0;
 }
 DEFINE_SIMPLE_ATTRIBUTE(enable_vmexit_trace_fops, NULL, enable_vmexit_trace, "%llu\n");